4ab86146d55e2a175baff910875e6baabd049579
Damien GERARD <damien@iwi.me>
2016-03-17 23:28:45 +0900

    the member objects must not be released after the 'clone' method

--- src/bootstrap/libnany/details/pass/build-instanciate/instanciate-member-variable-default-clone.cpp
+++ src/bootstrap/libnany/details/pass/build-instanciate/instanciate-member-variable-default-clone.cpp
@@ -24,11 +24,12 @@ namespace Instanciate
 		auto& parentAtom = *(frame->atom.parent);
 
 		// do not warn about rhs (unused). The parameter is used, but not via its name
-		// reminder: 1-based, 1: returntype, 2: self, 3: first parameter
+		// reminder: 1-based, 1: returntype, 2: self, 3: first parameter rhs
 		assert(3 < frame->lvids.size());
 		frame->lvids[3].warning.unused = false;
 
 
+		// looking for all members to clone
 		std::vector<std::reference_wrapper<Atom>> atomvars;
 		Atom* userDefinedClone = nullptr;
 
@@ -48,6 +49,7 @@ namespace Instanciate
 		if (atomvars.empty() and userDefinedClone == nullptr)
 			return;
 
+
 		// create new local variables for performing the cline
 		uint32_t more = (uint32_t)atomvars.size() * 2 + (userDefinedClone ? 1 : 0);
 		uint32_t lvid = createLocalVariables(more);
@@ -56,42 +58,63 @@ namespace Instanciate
 		{
 			auto& subatom = subatomref.get();
 			auto& cdef    = cdeftable.classdef(subatom.returnType.clid); // type of the var member
-			// out.emitComment(String{"clone for '"} << subatom.name << '\'');
+			if (debugmode)
+				out.emitComment(String{"\nCLONE for '"} << subatom.name << '\'');
 
-			if (not cdef.isBuiltinOrVoid())
+			switch (cdef.kind)
 			{
-				// read the pointer rhs
-				uint32_t rhsptr = lvid++;
-				uint32_t lhsptr = lvid++;
-				out.emitFieldget(rhsptr, /*rhs*/ 3, subatom.varinfo.effectiveFieldIndex);
+				case nyt_any:
+				{
+					uint32_t rhsptr = lvid++; // rhs value, from the object being cloned
+					uint32_t lhsptr = lvid++; // the target local value
 
-				auto& origin  = frame->lvids[rhsptr].origin.varMember;
-				origin.self   = 2;
-				origin.atomid = subatom.atomid;
-				origin.field  = subatom.varinfo.effectiveFieldIndex;
+					auto& origin  = frame->lvids[rhsptr].origin.varMember;
+					origin.self   = 2;
+					origin.atomid = subatom.atomid;
+					origin.field  = subatom.varinfo.effectiveFieldIndex;
 
-				auto& cdeflhs = cdeftable.substitute(lhsptr);
-				cdeflhs.import(cdef);
-				cdeflhs.qualifiers = cdef.qualifiers; // qualifiers must be preserved
+					auto& cdeflhs = cdeftable.substitute(lhsptr);
+					cdeflhs.import(cdef);
+					cdeflhs.qualifiers = cdef.qualifiers; // qualifiers must be preserved
 
-				auto& cdefrhs = cdeftable.substitute(rhsptr);
-				cdefrhs.import(cdef);
-				cdefrhs.qualifiers = cdef.qualifiers;
+					auto& cdefrhs = cdeftable.substitute(rhsptr);
+					cdefrhs.import(cdef);
+					cdefrhs.qualifiers = cdef.qualifiers;
 
-				instanciateAssignment(*frame, lhsptr, rhsptr, false);
-				out.emitFieldset(lhsptr, /*self*/ 2, subatom.varinfo.effectiveFieldIndex);
-			}
-			else
-			{
-				out.emitFieldget(lvid,  /*rhs*/  3, subatom.varinfo.effectiveFieldIndex);
-				out.emitFieldset(lvid, /*self*/ 2, subatom.varinfo.effectiveFieldIndex);
-				++lvid;
+					// fetching the rhs value, from the object being copied
+					out.emitFieldget(rhsptr, /*rhs*/ 3, subatom.varinfo.effectiveFieldIndex);
+
+					// perform a deep copy to the local variable
+					instanciateAssignment(*frame, lhsptr, rhsptr, false);
+					// .. copied to the member
+					out.emitFieldset(lhsptr, /*self*/ 2, subatom.varinfo.effectiveFieldIndex);
+
+					// prevent the cloned object from being released at the end of the scope
+					assert(canBeAcquired(lhsptr));
+					frame->lvids[lhsptr].autorelease = false;
+					break;
+				}
+				case nyt_void:
+				{
+					ICE() << "unexpected pseudo type 'void' for " << cdef.clid;
+					break;
+				}
+				default:
+				{
+					// rhs value, from the object being clone
+					out.emitFieldget(lvid, /*rhs*/  3, subatom.varinfo.effectiveFieldIndex);
+					// .. copied directly into the local member
+					out.emitFieldset(lvid, /*self*/ 2, subatom.varinfo.effectiveFieldIndex);
+					++lvid;
+				}
 			}
 		}
 
 		// call to user-defined operator dispose
 		if (userDefinedClone)
 		{
+			if (debugmode)
+				out.emitComment("\nuser's defined clone method");
 			out.emitPush(2); // self
 			out.emitPush(3); // rhs
 			out.emitCall(lvid, userDefinedClone->atomid, 0);
