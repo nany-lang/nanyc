b86edec4db3767325c32a1cf2cddac304a5a5bf3
Damien GERARD <damien@iwi.me>
2016-03-21 11:21:30 +0900

    ast-to-ir: code cleanup / typo

--- src/bootstrap/libnany/details/pass/build-ast-to-ir/source-ast-to-ir.cpp
+++ src/bootstrap/libnany/details/pass/build-ast-to-ir/source-ast-to-ir.cpp
@@ -16,26 +16,27 @@ namespace Nany
 	bool Source::passTransformASTToIRWL(Logs::Report& report)
 	{
 		auto& buildinfo = *pBuildInfo;
-		if (!buildinfo.parsing.rootnode)
+		if (nullptr == buildinfo.parsing.rootnode)
 			return false;
-		auto& astnodes = buildinfo.parsing.rootnode->children;
-
-		// helper for generating IR code
-		IR::Producer::Context producer{buildinfo.parsing.sequence, report};
-		// generate namespace-related opcodes
-		producer.useNamespace(buildinfo.parsing.nmspc.first);
-		producer.dbgSourceFilename = pFilename;
-		// map code offset (in bytes) with line numbers (from source input)
-		producer.generateLineIndexes(pContent);
 
 		bool success = true;
-
-		// generate IR code for all AST nodes
-		if (likely(not astnodes.empty()))
+		auto& astnodes = buildinfo.parsing.rootnode->children;
+		if (not astnodes.empty())
 		{
+			// output IR sequence
+			auto& out = buildinfo.parsing.sequence;
+
+			// helper for generating IR code
+			IR::Producer::Context producer{out, report};
+			// generate namespace-related opcodes
+			producer.useNamespace(buildinfo.parsing.nmspc.first);
+			producer.dbgSourceFilename = pFilename;
+			// map code offset (in bytes) with line numbers (from source input)
+			producer.generateLineIndexes(pContent);
+
+			// generate IR code for all AST nodes
 			IR::Producer::Scope scope{producer};
 			scope.addDebugCurrentFilename();
-			auto& out = scope.sequence();
 			uint32_t bpoffset = out.emitBlueprintUnit(pFilename);
 			uint32_t bpoffsiz = out.emitBlueprintSize();
 			uint32_t bpoffsck = out.emitStackSizeIncrease();
@@ -47,12 +48,12 @@ namespace Nany
 			uint32_t blpsize = out.opcodeCount() - bpoffset;
 			out.at<IR::ISA::Op::pragma>(bpoffsiz).value.blueprintsize = blpsize;
 			scope.sequence().at<IR::ISA::Op::stacksize>(bpoffsck).add = scope.nextvar();
-		}
 
-		// do not keep back information
-		buildinfo.parsing.parser.clear();
-		pContent.clear();
-		pContent.shrink();
+			// do not keep back information
+			buildinfo.parsing.parser.clear();
+			pContent.clear();
+			pContent.shrink();
+		}
 		return success;
 	}
 
