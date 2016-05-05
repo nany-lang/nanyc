ffd678e53ce19a9fbf4177c96f5beb0204123040
Damien GERARD <damien@iwi.me>
2016-05-05 16:05:09 +0900

    nany: added anonymous namespace

--- src/bootstrap/nany.cpp
+++ src/bootstrap/nany.cpp
@@ -49,150 +49,157 @@ namespace std
 } // namespace std
 
 
-struct Options
+
+namespace // anonymous
 {
-	//! Verbose mode
-	bool verbose = false;
-	//! Maximum number of jobs
-	uint32_t jobs = 1;
-	//! Memory limit (zero means unlimited)
-	size_t memoryLimit = 0;
 
-	//! The new argv0
-	String argv0;
-};
-
-
-static int printUsage(const char* const argv0)
-{
-	std::cout
-		<< "Usage: " << argv0 << " [options] file...\n"
-		<< "Options:\n"
-		<< "  --bugreport       Display some useful information to report a bug\n"
-		<< "                    (https://github.com/nany-lang/nany/issues/new)\n"
-		<< "  --help, -h        Display this information\n"
-		<< "  --version, -v     Print the version\n\n";
-	return EXIT_SUCCESS;
-}
-
-
-static int printBugReportInfo()
-{
-	auto* text = nany_get_info_for_bugreport();
-	if (text)
+	struct Options
 	{
-		std::cout << text;
-		::free(text);
-		return 0;
+		//! Verbose mode
+		bool verbose = false;
+		//! Maximum number of jobs
+		uint32_t jobs = 1;
+		//! Memory limit (zero means unlimited)
+		size_t memoryLimit = 0;
+
+		//! The new argv0
+		String argv0;
+	};
+
+
+	static int printUsage(const char* const argv0)
+	{
+		std::cout
+			<< "Usage: " << argv0 << " [options] file...\n"
+			<< "Options:\n"
+			<< "  --bugreport       Display some useful information to report a bug\n"
+			<< "                    (https://github.com/nany-lang/nany/issues/new)\n"
+			<< "  --help, -h        Display this information\n"
+			<< "  --version, -v     Print the version\n\n";
+		return EXIT_SUCCESS;
 	}
-	return EXIT_FAILURE;
-}
 
 
-static int printVersion()
-{
-	std::cout << nany_version() << '\n';
-	return EXIT_SUCCESS;
-}
-
-
-static int unknownOption(const AnyString& name)
-{
-	std::cerr << argv0 << ": unknown option '" << name << "'\n";
-	return EXIT_FAILURE;
-}
-
-
-static inline std::unique_ptr<nyproject_t> createProject(const Options& options)
-{
-	nyproject_cf_t cf;
-	nany_project_cf_init(&cf);
-
-	size_t limit = options.memoryLimit;
-	if (limit == 0)
-		limit = static_cast<size_t>(System::Environment::ReadAsUInt64("NANY_MEMORY_LIMIT"));
-
-	if (limit != 0)
-		nany_memalloc_set_with_limit(&cf.allocator, limit);
-
-	return std::unique_ptr<nyproject_t>{nany_project_create(&cf)};
-}
-
-
-static inline std::unique_ptr<nyprogram_t> compile(const AnyString& argv0, Options& options)
-{
-	// PROJECT
-	auto project = createProject(options);
-	if (!project)
-		return nullptr;
-
-	// SOURCE
-	auto& file = options.argv0;
-	IO::Canonicalize(file, argv0);
-	auto r = nany_project_add_source_from_file_n(project.get(), file.c_str(), file.size());
-	if (unlikely(r == nyfalse))
-		return nullptr;
-
-	// BUILD
-	nybuild_cf_t cf;
-	nany_build_cf_init(&cf, project.get());
-	auto build = std::unique_ptr<nybuild_t>{nany_build_prepare(project.get(), &cf)};
-	if (!build)
-		return nullptr;
-
-	do
+	static int printBugReportInfo()
 	{
-		auto bStatus = nany_build(build.get());
-		if (unlikely(bStatus == nyfalse))
-			break;
-
-		if (unlikely(options.verbose))
-			nany_build_print_report_to_console(build.get());
-
-		nyprogram_cf_t pcf;
-		nany_program_cf_init(&pcf, &cf);
-		return std::unique_ptr<nyprogram_t>(nany_program_prepare(build.get(), &pcf));
-	}
-	while (false);
-
-	// an error has occured
-	nany_build_print_report_to_console(build.get());
-	return nullptr;
-}
-
-
-static inline int execute(int argc, char** argv, Options& options)
-{
-	assert(argc >= 1);
-
-	auto program = compile(argv[0], options);
-	int exitstatus = 66;
-
-	if (!!program)
-	{
-		if (argc == 1)
+		auto* text = nany_get_info_for_bugreport();
+		if (text)
 		{
-			const char* nargv[] = { options.argv0.c_str(), nullptr };
-			exitstatus = nany_main(program.get(), 1, nargv);
+			std::cout << text;
+			::free(text);
+			return 0;
 		}
-		else
-		{
-			auto** nargv = (const char**)::malloc(((size_t) argc + 1) * sizeof(const char**));
-			if (nargv)
-			{
-				nargv[0] = options.argv0.c_str();
-				for (int i = 1; i < argc; ++i)
-					nargv[i] = argv[i];
-				nargv[argc] = nullptr;
+		return EXIT_FAILURE;
+	}
 
-				exitstatus = nany_main(program.get(), argc, nargv);
-				free(nargv);
+
+	static int printVersion()
+	{
+		std::cout << nany_version() << '\n';
+		return EXIT_SUCCESS;
+	}
+
+
+	static int unknownOption(const AnyString& name)
+	{
+		std::cerr << argv0 << ": unknown option '" << name << "'\n";
+		return EXIT_FAILURE;
+	}
+
+
+	static inline std::unique_ptr<nyproject_t> createProject(const Options& options)
+	{
+		nyproject_cf_t cf;
+		nany_project_cf_init(&cf);
+
+		size_t limit = options.memoryLimit;
+		if (limit == 0)
+			limit = static_cast<size_t>(System::Environment::ReadAsUInt64("NANY_MEMORY_LIMIT"));
+
+		if (limit != 0)
+			nany_memalloc_set_with_limit(&cf.allocator, limit);
+
+		return std::unique_ptr<nyproject_t>{nany_project_create(&cf)};
+	}
+
+
+	static inline std::unique_ptr<nyprogram_t> compile(const AnyString& argv0, Options& options)
+	{
+		// PROJECT
+		auto project = createProject(options);
+		if (!project)
+			return nullptr;
+
+		// SOURCE
+		auto& file = options.argv0;
+		IO::Canonicalize(file, argv0);
+		auto r = nany_project_add_source_from_file_n(project.get(), file.c_str(), file.size());
+		if (unlikely(r == nyfalse))
+			return nullptr;
+
+		// BUILD
+		nybuild_cf_t cf;
+		nany_build_cf_init(&cf, project.get());
+		auto build = std::unique_ptr<nybuild_t>{nany_build_prepare(project.get(), &cf)};
+		if (!build)
+			return nullptr;
+
+		do
+		{
+			auto bStatus = nany_build(build.get());
+			if (unlikely(bStatus == nyfalse))
+				break;
+
+			if (unlikely(options.verbose))
+				nany_build_print_report_to_console(build.get());
+
+			nyprogram_cf_t pcf;
+			nany_program_cf_init(&pcf, &cf);
+			return std::unique_ptr<nyprogram_t>(nany_program_prepare(build.get(), &pcf));
+		}
+		while (false);
+
+		// an error has occured
+		nany_build_print_report_to_console(build.get());
+		return nullptr;
+	}
+
+
+	static inline int execute(int argc, char** argv, Options& options)
+	{
+		assert(argc >= 1);
+
+		auto program = compile(argv[0], options);
+		int exitstatus = 66;
+
+		if (!!program)
+		{
+			if (argc == 1)
+			{
+				const char* nargv[] = { options.argv0.c_str(), nullptr };
+				exitstatus = nany_main(program.get(), 1, nargv);
+			}
+			else
+			{
+				auto** nargv = (const char**)::malloc(((size_t) argc + 1) * sizeof(const char**));
+				if (nargv)
+				{
+					nargv[0] = options.argv0.c_str();
+					for (int i = 1; i < argc; ++i)
+						nargv[i] = argv[i];
+					nargv[argc] = nullptr;
+
+					exitstatus = nany_main(program.get(), argc, nargv);
+					free(nargv);
+				}
 			}
 		}
+
+		return exitstatus;
 	}
 
-	return exitstatus;
-}
+
+} // anonymous namespace
 
 
 
