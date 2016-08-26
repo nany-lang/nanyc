f15e41d99b1e43c148be1911ec52b3a210effa5f
Damien GERARD <damien@iwi.me>
2016-08-27 01:11:47 +0900

    std.digest.md5: fixed invalid digest for empty imput

--- src/bootstrap/libnanyc/details/vm/runtime/std.digest.cpp
+++ src/bootstrap/libnanyc/details/vm/runtime/std.digest.cpp
@@ -12,17 +12,14 @@ using namespace Yuni;
 static void* nanyc_digest_md5(nyvm_t* vm, const char* string, uint64_t length)
 {
 	auto& tc = *reinterpret_cast<Nany::VM::ThreadContext*>(vm->tctx);
-	if (string and length)
+	Hash::Checksum::MD5 md5;
+	md5.fromRawData(string, length);
+	if (not md5.value().empty())
 	{
-		Hash::Checksum::MD5 md5;
-		md5.fromRawData(string, length);
-		if (not md5.value().empty())
-		{
-			tc.returnValue.size     = md5.value().size();
-			tc.returnValue.capacity = md5.value().capacity();
-			tc.returnValue.data     = md5.forgetContent();
-			return &tc.returnValue;
-		}
+		tc.returnValue.size     = md5.value().size();
+		tc.returnValue.capacity = md5.value().capacity();
+		tc.returnValue.data     = md5.forgetContent();
+		return &tc.returnValue;
 	}
 	return nullptr;
 }
