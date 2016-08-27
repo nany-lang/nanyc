494c8ec0aa012366e3f873b77712871a6111a99c
Damien GERARD <damien@iwi.me>
2016-08-27 13:40:04 +0900

    ast2ir: using heap allocation to reduce stack size

--- src/bootstrap/libnanyc/details/pass/build-ast-to-ir/source-ast-to-ir.cpp
+++ src/bootstrap/libnanyc/details/pass/build-ast-to-ir/source-ast-to-ir.cpp
@@ -19,40 +19,40 @@ namespace Nany
 		if (nullptr == buildinfo.parsing.rootnode)
 			return false;
 
-		bool success = true;
 		auto& astnodes = buildinfo.parsing.rootnode->children;
-		if (not astnodes.empty())
-		{
-			// output IR sequence
-			auto& out = buildinfo.parsing.sequence;
+		if (unlikely(astnodes.empty()))
+			return true;
 
-			// helper for generating IR code
-			IR::Producer::Context producer{buildinfo.cf, pFilename, out, report};
-			// generate namespace-related opcodes
-			producer.useNamespace(buildinfo.parsing.nmspc.first);
-			// map code offset (in bytes) with line numbers (from source input)
-			producer.generateLineIndexes(pContent);
+		// output IR sequence
+		auto& out = buildinfo.parsing.sequence;
 
-			// generate IR code for all AST nodes
-			IR::Producer::Scope scope{producer};
-			scope.addDebugCurrentFilename();
-			uint32_t bpoffset = out.emitBlueprintUnit(pFilename);
-			uint32_t bpoffsiz = out.emitBlueprintSize();
-			uint32_t bpoffsck = out.emitStackSizeIncrease();
+		// helper for generating IR code
+		auto producer = std::make_unique<IR::Producer::Context>(buildinfo.cf, pFilename, out, report);
+		// generate namespace-related opcodes
+		producer->useNamespace(buildinfo.parsing.nmspc.first);
+		// map code offset (in bytes) with line numbers (from source input)
+		producer->generateLineIndexes(pContent);
 
-			for (auto& element: astnodes)
-				success &= scope.visitAST(*element);
+		// generate IR code for all AST nodes
+		IR::Producer::Scope scope{*producer};
+		scope.addDebugCurrentFilename();
+		uint32_t bpoffset = out.emitBlueprintUnit(pFilename);
+		uint32_t bpoffsiz = out.emitBlueprintSize();
+		uint32_t bpoffsck = out.emitStackSizeIncrease();
 
-			out.emitEnd();
-			uint32_t blpsize = out.opcodeCount() - bpoffset;
-			out.at<IR::ISA::Op::pragma>(bpoffsiz).value.blueprintsize = blpsize;
-			scope.sequence().at<IR::ISA::Op::stacksize>(bpoffsck).add = scope.nextvar();
+		bool success = true;
+		for (uint32_t i = 0; i != static_cast<uint32_t>(astnodes.size()); ++i)
+			success &= scope.visitAST(*(astnodes[i]));
 
-			// do not keep back information
-			buildinfo.parsing.parser.clear();
-			pContent.clear();
-			pContent.shrink();
-		}
+		out.emitEnd();
+		uint32_t blpsize = out.opcodeCount() - bpoffset;
+		out.at<IR::ISA::Op::pragma>(bpoffsiz).value.blueprintsize = blpsize;
+		scope.sequence().at<IR::ISA::Op::stacksize>(bpoffsck).add = scope.nextvar();
+
+		// do not keep back information
+		buildinfo.parsing.parser.clear();
+		pContent.clear();
+		pContent.shrink();
 		return success;
 	}
 
