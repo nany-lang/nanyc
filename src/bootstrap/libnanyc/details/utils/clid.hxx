9c752ea31e666e1fd7e41bd1f82e99c00fe82663
Damien GERARD <damien@iwi.me>
2016-09-13 00:34:09 +0900

    simplified CLID on all platforms

--- src/bootstrap/libnanyc/details/utils/clid.hxx
+++ src/bootstrap/libnanyc/details/utils/clid.hxx
@@ -14,12 +14,7 @@ namespace Nany
 
 
 	inline CLID::CLID(uint32_t atomid, uint32_t lvid)
-		#if !LIBYUNI_CLID_UNION
-		: m_atomid(atomid)
-		, m_lvid(lvid)
-		#else
 		: m_data{{atomid, lvid}}
-		#endif
 	{
 		assert(lvid < 1000000); // arbitrary integrity check
 	}
@@ -27,106 +22,64 @@ namespace Nany
 
 	inline bool CLID::isVoid() const
 	{
-		#if !LIBYUNI_CLID_UNION
-		return !m_atomid and !m_lvid;
-		#else
 		return !m_data.u64;
-		#endif
 	}
 
 
 	inline void CLID::reclass(uint32_t lvid)
 	{
 		assert(lvid < 1000000); // arbitrary integrity check
-		#if !LIBYUNI_CLID_UNION
-		m_lvid = lvid;
-		#else
 		m_data.u32[1] = lvid;
-		#endif
 	}
 
 
 	inline void CLID::reclass(uint32_t atomid, uint32_t lvid)
 	{
 		assert(lvid < 1000000); // arbitrary integrity check
-		#if !LIBYUNI_CLID_UNION
-		m_atomid = atomid;
-		m_lvid = lvid;
-		#else
 		m_data.u32[0] = atomid;
 		m_data.u32[1] = lvid;
-		#endif
 	}
 
 
 	inline void CLID::reclassToVoid()
 	{
-		#if !LIBYUNI_CLID_UNION
-		m_atomid = 0;
-		m_lvid = 0;
-		#else
 		m_data.u64 = 0;
-		#endif
 	}
 
 
 	inline bool CLID::operator == (const CLID& rhs) const
 	{
-		#if !LIBYUNI_CLID_UNION
-		return m_atomid == rhs.m_atomid and m_lvid == rhs.m_lvid;
-		#else
 		return m_data.u64 == rhs.m_data.u64;
-		#endif
 	}
 
 
 	inline bool CLID::operator != (const CLID& rhs) const
 	{
-		#if !LIBYUNI_CLID_UNION
-		return m_atomid != rhs.m_atomid or m_lvid != rhs.m_lvid;
-		#else
 		return m_data.u64 != rhs.m_data.u64;
-		#endif
 	}
 
 
 	inline bool CLID::operator <  (const CLID& rhs) const
 	{
-		#if !LIBYUNI_CLID_UNION
-		return m_atomid < rhs.m_atomid and m_lvid < rhs.m_lvid;
-		#else
 		return m_data.u64 < rhs.m_data.u64;
-		#endif
 	}
 
 
 	inline size_t CLID::hash() const
 	{
-		#if !LIBYUNI_CLID_UNION
-		return std::hash<uint32_t>()(m_atomid) xor std::hash<uint32_t>()(m_lvid);
-		#else
 		return std::hash<uint64_t>()(m_data.u64);
-		#endif
 	}
 
 
 	inline uint32_t CLID::atomid() const
 	{
-		#if !LIBYUNI_CLID_UNION
-		return m_atomid;
-		#else
 		return m_data.u32[0];
-		#endif
 	}
 
 
 	inline uint32_t CLID::lvid() const
 	{
-		#if !LIBYUNI_CLID_UNION
-		return m_lvid;
-		#else
 		return m_data.u32[1];
-		#endif
 	}
 
 
