00d4ea6fd68ad66ac7be75e1192a819ca420070f
Damien GERARD <damien@iwi.me>
2016-10-05 00:29:25 +0900

    style: removed spurious indentation

--- src/bootstrap/libnanyc/details/pass/build-instanciate/instanciate-pragma.cpp
+++ src/bootstrap/libnanyc/details/pass/build-instanciate/instanciate-pragma.cpp
@@ -13,162 +13,162 @@ namespace Instanciate
 {
 
 
-	namespace // anonymous
+	namespace {
+
+
+	void pragmaBodyStart(SequenceBuilder& seq)
 	{
+		assert(seq.frame != nullptr);
+		assert(not seq.signatureOnly);
+		assert(seq.codeGenerationLock == 0 and "any good reason to not generate code ?");
+		assert(seq.out != nullptr and "no output IR sequence");
 
-		void pragmaBodyStart(SequenceBuilder& seq)
+		auto& frame = *seq.frame;
+		auto& atom = frame.atom;
+		if (not atom.isFunction())
+			return;
+
+		bool generateCode = seq.canGenerateCode();
+		uint32_t count = atom.parameters.size();
+
+		atom.parameters.each([&](uint32_t i, const AnyString& name, const Vardef& vardef)
 		{
-			assert(seq.frame != nullptr);
-			assert(not seq.signatureOnly);
-			assert(seq.codeGenerationLock == 0 and "any good reason to not generate code ?");
-			assert(seq.out != nullptr and "no output IR sequence");
+			// lvid for the given parameter
+			uint32_t lvid  = i + 1 + 1; // 1: return type, 2: first parameter
+			assert(lvid < frame.lvids.size());
+			// Obviously, the parameters are not synthetic objects
+			// but real variables
+			frame.lvids[lvid].synthetic = false;
 
-			auto& frame = *seq.frame;
-			auto& atom = frame.atom;
-			if (not atom.isFunction())
-				return;
-
-			bool generateCode = seq.canGenerateCode();
-			uint32_t count = atom.parameters.size();
-
-			atom.parameters.each([&](uint32_t i, const AnyString& name, const Vardef& vardef)
+			//
+			// Parameters Deep copy (if required)
+			//
+			if (name[0] != '^')
 			{
-				// lvid for the given parameter
-				uint32_t lvid  = i + 1 + 1; // 1: return type, 2: first parameter
-				assert(lvid < frame.lvids.size());
-				// Obviously, the parameters are not synthetic objects
-				// but real variables
-				frame.lvids[lvid].synthetic = false;
+				// normal input parameter (not captured - does not start with '^')
+				// clone it if necessary (only non-ref parameters)
 
-				//
-				// Parameters Deep copy (if required)
-				//
-				if (name[0] != '^')
+				if (not seq.cdeftable.classdef(vardef.clid).qualifiers.ref)
 				{
-					// normal input parameter (not captured - does not start with '^')
-					// clone it if necessary (only non-ref parameters)
+					if (debugmode and generateCode)
+						seq.out->emitComment(String{"----- deep copy parameter "} << i << " aka " << name);
 
-					if (not seq.cdeftable.classdef(vardef.clid).qualifiers.ref)
+					// a register has already been reserved for cloning parameters
+					uint32_t clone = 2 + count + i; // 1: return type, 2: first parameter
+					assert(clone < frame.lvids.size());
+					// the new value is not synthetic
+					frame.lvids[clone].synthetic = false;
+
+					bool r = seq.instanciateAssignment(frame, clone, lvid, false, false, true);
+					if (unlikely(not r))
+						frame.invalidate(clone);
+
+					if (seq.canBeAcquired(lvid))
 					{
-						if (debugmode and generateCode)
-							seq.out->emitComment(String{"----- deep copy parameter "} << i << " aka " << name);
+						frame.lvids[lvid].autorelease = true;
+						frame.lvids[clone].autorelease = false;
+					}
 
-						// a register has already been reserved for cloning parameters
-						uint32_t clone = 2 + count + i; // 1: return type, 2: first parameter
-						assert(clone < frame.lvids.size());
-						// the new value is not synthetic
-						frame.lvids[clone].synthetic = false;
-
-						bool r = seq.instanciateAssignment(frame, clone, lvid, false, false, true);
-						if (unlikely(not r))
-							frame.invalidate(clone);
-
-						if (seq.canBeAcquired(lvid))
-						{
-							frame.lvids[lvid].autorelease = true;
-							frame.lvids[clone].autorelease = false;
-						}
-
-						if (generateCode)
-						{
-							seq.out->emitStore(lvid, clone); // register swap
-							if (debugmode)
-								seq.out->emitComment("--\n");
-						}
+					if (generateCode)
+					{
+						seq.out->emitStore(lvid, clone); // register swap
+						if (debugmode)
+							seq.out->emitComment("--\n");
 					}
 				}
-			});
-
-			// generating some code on the fly
-			if (atom.isSpecial() /*ctor, operators...*/ and generateCode)
-			{
-				// variables initialization (for a ctor)
-				if (seq.generateClassVarsAutoInit)
-				{
-					seq.generateClassVarsAutoInit = false;
-					seq.generateMemberVarDefaultInitialization();
-				}
-
-				// variables destruction (for dtor)
-				if (seq.generateClassVarsAutoRelease)
-				{
-					seq.generateClassVarsAutoRelease = false;
-					seq.generateMemberVarDefaultDispose();
-				}
-
-				// variables cloning (copy a ctor)
-				if (seq.generateClassVarsAutoClone)
-				{
-					seq.generateClassVarsAutoClone = false;
-					seq.generateMemberVarDefaultClone();
-				}
 			}
-		}
+		});
 
-
-		void pragmaCodegen(SequenceBuilder& seq, bool onoff)
+		// generating some code on the fly
+		if (atom.isSpecial() /*ctor, operators...*/ and generateCode)
 		{
-			auto& refcount = seq.codeGenerationLock;
-			if (onoff)
+			// variables initialization (for a ctor)
+			if (seq.generateClassVarsAutoInit)
 			{
-				if (refcount > 0) // re-enable code generation
-					--refcount;
+				seq.generateClassVarsAutoInit = false;
+				seq.generateMemberVarDefaultInitialization();
 			}
-			else
-				++refcount;
+
+			// variables destruction (for dtor)
+			if (seq.generateClassVarsAutoRelease)
+			{
+				seq.generateClassVarsAutoRelease = false;
+				seq.generateMemberVarDefaultDispose();
+			}
+
+			// variables cloning (copy a ctor)
+			if (seq.generateClassVarsAutoClone)
+			{
+				seq.generateClassVarsAutoClone = false;
+				seq.generateMemberVarDefaultClone();
+			}
 		}
+	}
 
 
-		void pragmaBlueprintSize(SequenceBuilder& seq, uint32_t opcodeCount)
+	void pragmaCodegen(SequenceBuilder& seq, bool onoff)
+	{
+		auto& refcount = seq.codeGenerationLock;
+		if (onoff)
 		{
-			if (0 == seq.layerDepthLimit)
-			{
-				// ignore the current blueprint
-				//*cursor += operands.value.blueprintsize;
-				assert(seq.frame != nullptr);
-				assert(seq.frame->offsetOpcodeBlueprint != (uint32_t) -1);
-				auto startOffset = seq.frame->offsetOpcodeBlueprint;
-				if (unlikely(opcodeCount < 3))
-					return (void) (ice() << "invalid blueprint size when instanciating atom");
-
-				// goto the end of the blueprint
-				// -2: the final 'end' opcode must be interpreted
-				*seq.cursor = &seq.currentSequence.at(startOffset + opcodeCount - 1 - 1);
-			}
+			if (refcount > 0) // re-enable code generation
+				--refcount;
 		}
+		else
+			++refcount;
+	}
 
 
-		void pragmaShortcircuitMutateToBool(SequenceBuilder& seq, const IR::ISA::Operand<IR::ISA::Op::pragma>& operands)
+	void pragmaBlueprintSize(SequenceBuilder& seq, uint32_t opcodeCount)
+	{
+		if (0 == seq.layerDepthLimit)
 		{
-			uint32_t lvid = operands.value.shortcircuitMutate.lvid;
-			uint32_t source = operands.value.shortcircuitMutate.source;
+			// ignore the current blueprint
+			//*cursor += operands.value.blueprintsize;
+			assert(seq.frame != nullptr);
+			assert(seq.frame->offsetOpcodeBlueprint != (uint32_t) -1);
+			auto startOffset = seq.frame->offsetOpcodeBlueprint;
+			if (unlikely(opcodeCount < 3))
+				return (void) (ice() << "invalid blueprint size when instanciating atom");
 
-			seq.frame->lvids[lvid].synthetic = false;
-
-			if (true)
-			{
-				auto& instr = *(*seq.cursor - 1);
-				assert(instr.opcodes[0] == static_cast<uint32_t>(IR::ISA::Op::stackalloc));
-				uint32_t sizeoflvid = instr.to<IR::ISA::Op::stackalloc>().lvid;
-
-				// sizeof
-				auto& atombool = *(seq.cdeftable.atoms().core.object[nyt_bool]);
-				seq.out->emitSizeof(sizeoflvid, atombool.atomid);
-
-				auto& opc = seq.cdeftable.substitute(lvid);
-				opc.mutateToAtom(&atombool);
-				opc.qualifiers.ref = true;
-
-				// ALLOC: memory allocation of the new temporary object
-				seq.out->emitMemalloc(lvid, sizeoflvid);
-				seq.out->emitRef(lvid);
-				seq.frame->lvids[lvid].autorelease = true;
-				// reset the internal value of the object
-				seq.out->emitFieldset(source, /*self*/lvid, 0); // builtin
-			}
-			else
-				seq.out->emitStore(lvid, source);
+			// goto the end of the blueprint
+			// -2: the final 'end' opcode must be interpreted
+			*seq.cursor = &seq.currentSequence.at(startOffset + opcodeCount - 1 - 1);
 		}
+	}
+
+
+	void pragmaShortcircuitMutateToBool(SequenceBuilder& seq, const IR::ISA::Operand<IR::ISA::Op::pragma>& operands)
+	{
+		uint32_t lvid = operands.value.shortcircuitMutate.lvid;
+		uint32_t source = operands.value.shortcircuitMutate.source;
+
+		seq.frame->lvids[lvid].synthetic = false;
+
+		if (true)
+		{
+			auto& instr = *(*seq.cursor - 1);
+			assert(instr.opcodes[0] == static_cast<uint32_t>(IR::ISA::Op::stackalloc));
+			uint32_t sizeoflvid = instr.to<IR::ISA::Op::stackalloc>().lvid;
+
+			// sizeof
+			auto& atombool = *(seq.cdeftable.atoms().core.object[nyt_bool]);
+			seq.out->emitSizeof(sizeoflvid, atombool.atomid);
+
+			auto& opc = seq.cdeftable.substitute(lvid);
+			opc.mutateToAtom(&atombool);
+			opc.qualifiers.ref = true;
+
+			// ALLOC: memory allocation of the new temporary object
+			seq.out->emitMemalloc(lvid, sizeoflvid);
+			seq.out->emitRef(lvid);
+			seq.frame->lvids[lvid].autorelease = true;
+			// reset the internal value of the object
+			seq.out->emitFieldset(source, /*self*/lvid, 0); // builtin
+		}
+		else
+			seq.out->emitStore(lvid, source);
+	}
 
 
 	} // anonymous namespace
