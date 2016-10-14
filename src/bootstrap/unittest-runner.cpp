a44687078cdd923994e4889a6fa5892e8c549d8c
Damien GERARD <damien@iwi.me>
2016-10-15 00:57:40 +0900

    unittest: c++ refactor / cleanup

--- src/bootstrap/unittest-runner.cpp
+++ src/bootstrap/unittest-runner.cpp
@@ -6,6 +6,7 @@
 #include <yuni/core/system/console/console.h>
 #include <yuni/io/file.h>
 #include <set>
+#include <memory>
 #include <iostream>
 #include <cassert>
 #include <random>
@@ -17,29 +18,78 @@ using namespace Yuni;
 namespace
 {
 
-	static uint32_t jobCount = 0;
-	static bool hasColorsOut = false;
-	static bool hasColorsErr = false;
+	struct Settings final
+	{
+		std::vector<String> unittests;
+		std::vector<String> remainingArgs;
+		bool listAll = false;
+		bool nslTests = false;
+		bool shuffle = false;
+		uint32_t jobs = 0;
+		uint32_t repeat = 1;
+		struct {
+			bool out = false;
+			bool err = false;
+		}
+		colors;
+	};
 
 
-	int printBugReportInfo()
+	bool printBugReportInfo()
 	{
 		nylib_print_info_for_bugreport();
-		return EXIT_SUCCESS;
+		return false;
 	}
 
 
-	int printVersion()
+	bool printVersion()
 	{
 		std::cout << nylib_version() << '\n';
-		return EXIT_SUCCESS;
+		return false;
 	}
 
 
-	int printNoInputScript(const char* argv0)
+	void shuffleUnittests(std::vector<String>& unittests)
 	{
-		std::cerr << argv0 << ": no input script file\n";
-		return EXIT_FAILURE;
+		std::cout << "shuffling the tests...\n";
+		auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+		auto useed = static_cast<uint32_t>(seed);
+		std::shuffle(unittests.begin(), unittests.end(), std::default_random_engine(useed));
+	}
+
+
+	bool parseCommandLine(Settings& settings, int argc, char** argv)
+	{
+		GetOpt::Parser options;
+		options.addFlag(settings.listAll, 'l', "list", "List all unit tests");
+		options.addFlag(settings.unittests, 'r', "run", "Run a specific test");
+		options.add(settings.jobs, 'j', "job", "Specifies the number of jobs (commands) to run simultaneously");
+		options.addFlag(settings.shuffle, ' ', "shuffle", "Randomly rearrange the tests");
+		options.add(settings.repeat, ' ', "repeat", "Repeat the execution of the tests N times");
+		options.addFlag(settings.nslTests, ' ', "with-nsl", "Import NSL unittests");
+		options.remainingArguments(settings.remainingArgs);
+
+		options.addParagraph("\nHelp");
+		bool bugreport = false;
+		options.addFlag(bugreport, ' ', "bugreport", "Display some useful information to report a bug");
+		bool version = false;
+		options.addFlag(version, 'v', "version", "Print the version");
+
+		if (not options(argc, argv))
+		{
+			if (options.errors())
+				throw std::runtime_error("Abort due to error");
+			return false;
+		}
+
+		if (version)
+			return printVersion();
+		if (bugreport)
+			return printBugReportInfo();
+
+		if (settings.remainingArgs.empty())
+			throw std::runtime_error(std::string{argv[0]} += ": no input script file");
+		return true;
 	}
 
 
@@ -74,7 +124,7 @@ namespace
 	}
 
 
-	int listAllUnittests(nyrun_cf_t& runcf, const char** filelist, uint32_t count)
+	int listAllUnittests(nyrun_cf_t& runcf, const Settings& settings, const char** filelist, uint32_t count)
 	{
 		std::set<std::pair<String, String>> alltests;
 
@@ -109,10 +159,10 @@ namespace
 			{
 				++moduleCount;
 				lastmodule = module;
-				if (hasColorsOut)
+				if (settings.colors.out)
 					System::Console::SetTextColor(std::cout, System::Console::bold);
 				std::cout << '.' << lastmodule << ":\n";
-				if (hasColorsOut)
+				if (settings.colors.out)
 					System::Console::ResetTextColor(std::cout);
 			}
 
@@ -123,7 +173,7 @@ namespace
 		}
 
 		std::cout << "\n       ";
-		if (hasColorsOut)
+		if (settings.colors.out)
 			System::Console::SetTextColor(std::cout, System::Console::lightblue);
 		switch (moduleCount)
 		{
@@ -137,7 +187,7 @@ namespace
 			case 1:  std::cout << "1 test found"; break;
 			default: std::cout << alltests.size() << " tests found"; break;
 		}
-		if (hasColorsOut)
+		if (settings.colors.out)
 			System::Console::ResetTextColor(std::cout);
 		std::cout << "  (" << duration << "ms)\n\n";
 		return EXIT_SUCCESS;
@@ -145,12 +195,12 @@ namespace
 
 
 	template<bool Fancy>
-	bool runtest(nyrun_cf_t& originalRuncf, const String& testname, const char** filelist, uint32_t count)
+	bool runtest(nyrun_cf_t& originalRuncf, const Settings& settings, const String& testname, const char** filelist, uint32_t count)
 	{
 		ShortString256 entry;
 		entry << "^unittest^" << testname;
 		entry.replace("<nomodule>", "module");
-		bool interactive = (hasColorsOut and jobCount == 1);
+		bool interactive = (settings.colors.out and settings.jobs == 1);
 
 		auto runcf = originalRuncf;
 		runcf.build.entrypoint.size  = entry.size();
@@ -158,12 +208,9 @@ namespace
 		runcf.program.entrypoint = runcf.build.entrypoint;
 		runcf.build.ignore_atoms = nyfalse;
 
-		struct Console
-		{
-			Clob cout;
-			Clob cerr;
-		}
-		console;
+		struct Console {
+			Clob cout, cerr;
+		} console;
 
 		runcf.console.release = nullptr;
 		runcf.console.internal = &console;
@@ -186,10 +233,10 @@ namespace
 
 		if (interactive and Fancy)
 		{
-			if (hasColorsOut)
+			if (settings.colors.out)
 				System::Console::SetTextColor(std::cout, System::Console::bold);
 			std::cout << "       running ";
-			if (hasColorsOut)
+			if (settings.colors.out)
 				System::Console::ResetTextColor(std::cout);
 			std::cout << testname << "... " << std::flush;
 		}
@@ -208,7 +255,7 @@ namespace
 
 			if (success)
 			{
-				if (hasColorsOut)
+				if (settings.colors.out)
 					System::Console::SetTextColor(std::cout, System::Console::green);
 				#ifndef YUNI_OS_WINDOWS
 				std::cout << "    \u2713  ";
@@ -218,17 +265,17 @@ namespace
 			}
 			else
 			{
-				if (hasColorsOut)
+				if (settings.colors.out)
 					System::Console::SetTextColor(std::cout, System::Console::red);
 				std::cout << "  ERR  ";
 			}
 
-			if (hasColorsOut)
+			if (settings.colors.out)
 				System::Console::ResetTextColor(std::cout);
 
 			std::cout << testname;
 
-			if (duration < 1200 or not hasColorsOut)
+			if (duration < 1200 or not settings.colors.out)
 			{
 				std::cout << "  (" << duration << "ms)     ";
 			}
@@ -253,15 +300,15 @@ namespace
 	}
 
 
-	void printStatstics(const std::vector<String>& optToRun, int64_t duration, uint32_t successCount, uint32_t failCount)
+	void printStatstics(const Settings& settings, int64_t duration, uint32_t successCount, uint32_t failCount)
 	{
-		switch (optToRun.size())
+		switch (settings.unittests.size())
 		{
 			case 0:  std::cout << "\n       0 test, "; break;
 			case 1:  std::cout << "\n       1 test"; break;
-			default: std::cout << "\n       " << optToRun.size() << " tests, "; break;
+			default: std::cout << "\n       " << settings.unittests.size() << " tests, "; break;
 		}
-		if (successCount and hasColorsOut)
+		if (successCount and settings.colors.out)
 			System::Console::SetTextColor(std::cout, System::Console::green);
 		switch (successCount)
 		{
@@ -269,12 +316,12 @@ namespace
 			case 1:  std::cout << "1 passing"; break;
 			default: std::cout << successCount << " passing"; break;
 		}
-		if (successCount and hasColorsOut)
+		if (successCount and settings.colors.out)
 			System::Console::ResetTextColor(std::cout);
 		if (failCount)
 			std::cout << ", ";
 
-		if (failCount and hasColorsOut)
+		if (failCount and settings.colors.out)
 			System::Console::SetTextColor(std::cout, System::Console::red);
 		switch (failCount)
 		{
@@ -282,7 +329,7 @@ namespace
 			case 1:  std::cout << "1 failed"; break;
 			default: std::cout << "" << failCount << " failed"; break;
 		}
-		if (failCount and hasColorsOut)
+		if (failCount and settings.colors.out)
 			System::Console::ResetTextColor(std::cout);
 
 		std::cout << "  (";
@@ -294,29 +341,25 @@ namespace
 	}
 
 
-	bool runUnittsts(nyrun_cf_t& runcf, const std::vector<String>& optToRun, const char** filelist, uint32_t filecount)
+	bool runUnittests(nyrun_cf_t& runcf, const Settings& settings, const char** filelist, uint32_t filecount)
 	{
-		if (optToRun.empty())
-		{
-			std::cerr << "error: no unit test name\n";
-			return false;
-		}
+		if (settings.unittests.empty())
+			throw std::runtime_error("error: no unit test name");
 
-		uint32_t successCount = 0;
-		uint32_t failCount = 0;
-		int64_t starttime = DateTime::NowMilliSeconds();
 		runcf.build.ignore_atoms = nytrue;
 		runcf.build.userdata = nullptr;
 		runcf.build.on_unittest = nullptr;
+		uint32_t successCount = 0;
+		uint32_t failCount = 0;
+		int64_t starttime = DateTime::NowMilliSeconds();
 
-		for (auto& testname: optToRun)
+		for (auto& testname: settings.unittests)
 		{
-			bool localsuccess = runtest<true>(runcf, testname, filelist, filecount);
+			bool localsuccess = runtest<true>(runcf, settings, testname, filelist, filecount);
 			++(localsuccess ? successCount : failCount);
 		}
-
 		int64_t duration = DateTime::NowMilliSeconds() - starttime;
-		printStatstics(optToRun, duration, successCount, failCount);
+		printStatstics(settings, duration, successCount, failCount);
 		return (failCount == 0 and successCount != 0);
 	}
 
@@ -336,12 +379,54 @@ namespace
 	}
 
 
-	void shuffleUnittests(std::vector<String>& unittests)
+	int runAllUnittests(nyrun_cf_t& runcf, Settings& settings, const char** filelist, uint32_t filecount)
 	{
-		std::cout << "shuffling the tests...\n";
-		auto seed = std::chrono::system_clock::now().time_since_epoch().count();
-		auto useed = static_cast<uint32_t>(seed);
-		std::shuffle(unittests.begin(), unittests.end(), std::default_random_engine(useed));
+		if (settings.colors.out)
+			System::Console::SetTextColor(std::cout, System::Console::bold);
+		std::cout << "nanyc C++/boostrap unittest " << nylib_version();
+		if (settings.colors.out)
+			System::Console::ResetTextColor(std::cout);
+		std::cout << '\n';
+		nylib_print_info_for_bugreport();
+
+		std::cout << ">\n";
+		for (uint32_t i = 0; i != filecount; ++i)
+			std::cout << "> from '" << filelist[i] << "'\n";
+		std::cout << '\n';
+
+		if (settings.jobs == 0 or settings.jobs > 128)
+			settings.jobs = 1; // System::CPU::Count();
+
+		// no unittest provided from the command - default: all
+		if (settings.unittests.empty())
+			fetchUnittestList(runcf, settings.unittests, filelist, filecount);
+
+		int exitcode = EXIT_SUCCESS;
+		for (uint32_t r = 0; r != settings.repeat; ++r)
+		{
+			if (settings.shuffle)
+				shuffleUnittests(settings.unittests);
+
+			if (settings.shuffle or settings.repeat > 1)
+			{
+				std::cout << "running " << settings.unittests.size() << " tests...";
+				if (settings.repeat > 1)
+				{
+					if (settings.colors.out)
+						System::Console::SetTextColor(std::cout, System::Console::bold);
+					std::cout << " (pass " << (r + 1) << '/' << settings.repeat << ')';
+					if (settings.colors.out)
+						System::Console::ResetTextColor(std::cout);
+				}
+				std::cout << '\n';
+			}
+			std::cout << '\n';
+
+			bool success = runUnittests(runcf, settings, filelist, filecount);
+			if (not success)
+				exitcode = EXIT_FAILURE;
+		}
+		return exitcode;
 	}
 
 
@@ -352,104 +437,29 @@ namespace
 
 int main(int argc, char** argv)
 {
-	std::vector<String> optToRun;
-	std::vector<String> remainingArgs;
-	bool optListAll = false;
-	bool optWithNSLTests = false;
-	bool optShuffle = false;
-	uint32_t optRepeat = 1;
-
-	// The command line options parser
+	try
 	{
-		GetOpt::Parser options;
-		options.addFlag(optListAll, 'l', "list", "List all unit tests");
-		options.addFlag(optToRun, 'r', "run", "Run a specific test");
-		options.add(jobCount, 'j', "job", "Specifies the number of jobs (commands) to run simultaneously");
-		options.addFlag(optShuffle, ' ', "shuffle", "Randomly rearrange the tests");
-		options.add(optRepeat, ' ', "repeat", "Repeat the execution of the tests N times");
-		options.addFlag(optWithNSLTests, ' ', "with-nsl", "Import NSL unittests");
-		options.remainingArguments(remainingArgs);
-
-		options.addParagraph("\nHelp");
-		bool optBugreport = false;
-		options.addFlag(optBugreport, ' ', "bugreport", "Display some useful information to report a bug");
-		bool optVersion = false;
-		options.addFlag(optVersion, 'v', "version", "Print the version");
-
-		if (not options(argc, argv))
-		{
-			if (options.errors())
-			{
-				std::cout << "Abort due to error\n";
-				return EXIT_FAILURE;
-			}
+		Settings settings;
+		if (not parseCommandLine(settings, argc, argv))
 			return EXIT_SUCCESS;
-		}
 
-		if (optVersion)
-			return printVersion();
-		if (optBugreport)
-			return printBugReportInfo();
+		auto filecount = static_cast<uint32_t>(settings.remainingArgs.size());
+		auto filelist = canonicalizeAllFilenames(settings.remainingArgs);
+		settings.colors.out = System::Console::IsStdoutTTY();
+		settings.colors.err = System::Console::IsStderrTTY();
+
+		nyrun_cf_t runcf;
+		nyrun_cf_init(&runcf);
+		if (settings.nslTests)
+			runcf.project.with_nsl_unittests = nytrue;
+
+		return (settings.listAll)
+			? listAllUnittests(runcf, settings, filelist.get(), filecount)
+			: runAllUnittests(runcf, settings, filelist.get(), filecount);
 	}
-
-	auto filecount = static_cast<uint32_t>(remainingArgs.size());
-	if (filecount == 0)
-		return printNoInputScript(argv[0]);
-	auto filelist = canonicalizeAllFilenames(remainingArgs);
-
-	nyrun_cf_t runcf;
-	nyrun_cf_init(&runcf);
-	if (optWithNSLTests)
-		runcf.project.with_nsl_unittests = nytrue;
-
-	int exitcode = EXIT_SUCCESS;
-	hasColorsOut = System::Console::IsStdoutTTY();
-	hasColorsErr = System::Console::IsStderrTTY();
-
-	if (optListAll)
+	catch (const std::exception& e)
 	{
-		exitcode = listAllUnittests(runcf, filelist.get(), filecount);
+		std::cerr << e.what() << '\n';
 	}
-	else
-	{
-		if (hasColorsOut)
-			System::Console::SetTextColor(std::cout, System::Console::bold);
-		std::cout << "nanyc C++/boostrap unittest " << nylib_version();
-		if (hasColorsOut)
-			System::Console::ResetTextColor(std::cout);
-		std::cout << '\n';
-		nylib_print_info_for_bugreport();
-
-		std::cout << ">\n";
-		for (uint32_t i = 0; i != filecount; ++i)
-			std::cout << "> from '" << filelist[i] << "'\n";
-		std::cout << '\n';
-
-		if (0 == jobCount or jobCount > 128)
-			jobCount = System::CPU::Count();
-
-		// no unittest provided from the command - default: all
-		if (optToRun.empty())
-			fetchUnittestList(runcf, optToRun, filelist.get(), filecount);
-
-		for (uint32_t r = 0; r != optRepeat; ++r)
-		{
-			if (optShuffle)
-				shuffleUnittests(optToRun);
-
-			if (optShuffle or optRepeat > 1)
-			{
-				std::cout << "running " << optToRun.size() << " tests...";
-				if (optRepeat > 1)
-					std::cout << " (pass " << (r + 1) << '/' << optRepeat << ')';
-				std::cout << '\n';
-			}
-			std::cout << '\n';
-
-			bool success = runUnittsts(runcf, optToRun, filelist.get(), filecount);
-			if (not success)
-				exitcode = EXIT_FAILURE;
-		}
-	}
-	return exitcode;
+	return EXIT_FAILURE;
 }
