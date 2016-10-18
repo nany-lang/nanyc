5132e7084d26df54d7bb85fc8854b7d64c3fd52c
Damien GERARD <damien@iwi.me>
2016-10-18 22:40:48 +0900

    instanciate: do not emit opcodes when asked to
    fixes a SegV with `typeof(<expr>)`

--- src/bootstrap/libnanyc/details/pass/build-instanciate/instanciate-store.cpp
+++ src/bootstrap/libnanyc/details/pass/build-instanciate/instanciate-store.cpp
@@ -16,7 +16,8 @@ namespace Instanciate
 	{
 		assert(frame != nullptr);
 		frame->lvids[operands.lvid].synthetic = false;
-		out->emitStore_u64(operands.lvid, operands.value.u64);
+		if (canGenerateCode())
+			out->emitStore_u64(operands.lvid, operands.value.u64);
 	}
 
 
@@ -36,16 +37,20 @@ namespace Instanciate
 			auto& spare = cdeftable.substitute(cdef.clid.lvid());
 			spare.import(cdefsrc);
 		}
-		out->emitStore(operands.lvid, operands.source);
+		if (canGenerateCode())
+			out->emitStore(operands.lvid, operands.source);
 	}
 
 
 	void SequenceBuilder::visit(const IR::ISA::Operand<IR::ISA::Op::storeText>& operands)
 	{
-		uint32_t sid = out->emitStoreText(operands.lvid, currentSequence.stringrefs[operands.text]);
-		auto& lvidinfo = frame->lvids[operands.lvid];
-		lvidinfo.synthetic = false;
-		lvidinfo.text_sid  = sid;
+		if (canGenerateCode())
+		{
+			uint32_t sid = out->emitStoreText(operands.lvid, currentSequence.stringrefs[operands.text]);
+			auto& lvidinfo = frame->lvids[operands.lvid];
+			lvidinfo.synthetic = false;
+			lvidinfo.text_sid  = sid;
+		}
 	}
 
 
