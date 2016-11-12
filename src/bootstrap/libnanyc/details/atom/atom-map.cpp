9302db64f69d33ac2280f06e01c379b4e398bc0b
Damien GERARD <damien@iwi.me>
2016-11-12 19:12:21 +0900

    atom map: removed empty lines

--- src/bootstrap/libnanyc/details/atom/atom-map.cpp
+++ src/bootstrap/libnanyc/details/atom/atom-map.cpp
@@ -42,9 +42,7 @@ namespace ny
 				break;
 			}
 			default:
-			{
 				error() << "multiple definition for 'class" << name << "'";
-			}
 		}
 		return false;
 	}
@@ -94,30 +92,23 @@ namespace ny
 
 	bool AtomMap::fetchAndIndexCoreObjects()
 	{
-		bool success = true;
+		bool success = (core.object[nyt_bool] != nullptr) // quick & arbitrary check
+			or
+			(findCoreObject(core.object[nyt_bool],   nyt_bool, "bool", root)
+			and findCoreObject(core.object[nyt_i8],  nyt_i8,   "i8",  root)
+			and findCoreObject(core.object[nyt_i16], nyt_i16,  "i16", root)
+			and findCoreObject(core.object[nyt_i32], nyt_i32,  "i32", root)
+			and findCoreObject(core.object[nyt_i64], nyt_i64,  "i64", root)
+			and findCoreObject(core.object[nyt_u8],  nyt_u8,   "u8",  root)
+			and findCoreObject(core.object[nyt_u16], nyt_u16,  "u16", root)
+			and findCoreObject(core.object[nyt_u32], nyt_u32,  "u32", root)
+			and findCoreObject(core.object[nyt_u64], nyt_u64,  "u64", root)
+			and findCoreObject(core.object[nyt_f32], nyt_f32,  "f32", root)
+			and findCoreObject(core.object[nyt_f64], nyt_f64,  "f64", root)
+			and findCoreObject(core.object[nyt_ptr], nyt_ptr,  "pointer", root));
 
-		if (core.object[nyt_bool] == nullptr) // quick & arbitrary check
-		{
-			success &= findCoreObject(core.object[nyt_bool], nyt_bool, "bool", root);
-
-			success &= findCoreObject(core.object[nyt_i8],  nyt_i8,  "i8",  root);
-			success &= findCoreObject(core.object[nyt_i16], nyt_i16, "i16", root);
-			success &= findCoreObject(core.object[nyt_i32], nyt_i32, "i32", root);
-			success &= findCoreObject(core.object[nyt_i64], nyt_i64, "i64", root);
-
-			success &= findCoreObject(core.object[nyt_u8],  nyt_u8,  "u8",  root);
-			success &= findCoreObject(core.object[nyt_u16], nyt_u16, "u16", root);
-			success &= findCoreObject(core.object[nyt_u32], nyt_u32, "u32", root);
-			success &= findCoreObject(core.object[nyt_u64], nyt_u64, "u64", root);
-
-			success &= findCoreObject(core.object[nyt_f32], nyt_f32, "f32", root);
-			success &= findCoreObject(core.object[nyt_f64], nyt_f64, "f64", root);
-
-			success &= findCoreObject(core.object[nyt_ptr], nyt_ptr, "pointer", root);
-
-			if (unlikely(not success))
-				core.object[nyt_bool] = nullptr;
-		}
+		if (unlikely(not success))
+			core.object[nyt_bool] = nullptr;
 		return success;
 	}
 
