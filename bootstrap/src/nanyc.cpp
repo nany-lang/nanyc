9d8374e965a17f93a9f96226cc9621bf8e5153e1
Damien GERARD <damien@iwi.me>
2016-11-20 14:02:01 +0900

    nanyc: reverse condition for early exit

--- bootstrap/src/nanyc.cpp
+++ bootstrap/src/nanyc.cpp
@@ -7,83 +7,81 @@ using namespace Yuni;
 
 int main(int argc, const char** argv)
 {
-	if (argc > 1)
+	if (!(argc > 1))
+		return ny::print::noInputScript(argv[0]);
+	nyrun_cf_t runcf;
+	nyrun_cf_init(&runcf);
+
+	int firstarg = argc; // end of the list
+
+	for (int i = 1; i < argc; ++i)
 	{
-		nyrun_cf_t runcf;
-		nyrun_cf_init(&runcf);
-
-		int firstarg = argc; // end of the list
-
-		for (int i = 1; i < argc; ++i)
+		const char* const carg = argv[i];
+		if (carg[0] == '-')
 		{
-			const char* const carg = argv[i];
-			if (carg[0] == '-')
+			if (carg[1] == '-')
 			{
-				if (carg[1] == '-')
+				if (carg[2] != '\0') // to handle '--' option
 				{
-					if (carg[2] != '\0') // to handle '--' option
-					{
-						AnyString arg{carg};
+					AnyString arg{carg};
 
-						if (arg == "--help")
-							return ny::print::usage(argv[0]);
-						if (arg == "--version")
-							return ny::print::version();
-						if (arg == "--bugreport")
-							return ny::print::bugReportInfo();
-						if (arg == "--verbose")
-						{
-							runcf.verbose = nytrue;
-							continue;
-						}
-						return ny::print::unknownOption(argv[0], arg);
-					}
-					else
+					if (arg == "--help")
+						return ny::print::usage(argv[0]);
+					if (arg == "--version")
+						return ny::print::version();
+					if (arg == "--bugreport")
+						return ny::print::bugReportInfo();
+					if (arg == "--verbose")
 					{
-						// nothing must interpreted after '--'
-						firstarg = i + 1;
-						break;
+						runcf.verbose = nytrue;
+						continue;
 					}
+					return ny::print::unknownOption(argv[0], arg);
 				}
 				else
 				{
-					AnyString arg{carg};
-					if (arg == "-h")
-						return ny::print::usage(argv[0]);
-					if (arg == "-v")
-						return ny::print::version();
-					return ny::print::unknownOption(argv[0], arg);
+					// nothing must interpreted after '--'
+					firstarg = i + 1;
+					break;
 				}
 			}
-			firstarg = i;
-			break;
+			else
+			{
+				AnyString arg{carg};
+				if (arg == "-h")
+					return ny::print::usage(argv[0]);
+				if (arg == "-v")
+					return ny::print::version();
+				return ny::print::unknownOption(argv[0], arg);
+			}
 		}
-
-
-		//
-		// -- execute the script
-		//
-		int exitstatus = -1;
-		if (firstarg < argc)
-		{
-			// callbacks
-			runcf.build.on_error_file_eacces = &ny::print::fileAccessError;
-
-			// the new arguments, after removing all command line arguments
-			int nargc = argc - firstarg;
-			const char** nargv = argv + firstarg;
-			// the nany sript to load, which should be the first new argument
-			const char* nargv0 = nargv[0];
-
-			--nargc;
-			uint32_t pargc = (nargc > 0) ? static_cast<uint32_t>(nargc) : 0;
-			const char** pargv = (!pargc ? nullptr : (++nargv));
-
-			exitstatus = nyrun_file_n(&runcf, nargv0, strlen(nargv0), pargc, pargv);
-		}
-
-		nyrun_cf_release(&runcf);
-		return exitstatus;
+		firstarg = i;
+		break;
 	}
-	return ny::print::noInputScript(argv[0]);
+
+
+	//
+	// -- execute the script
+	//
+	int exitstatus = -1;
+	if (firstarg < argc)
+	{
+		// callbacks
+		runcf.build.on_error_file_eacces = &ny::print::fileAccessError;
+
+		// the new arguments, after removing all command line arguments
+		int nargc = argc - firstarg;
+		const char** nargv = argv + firstarg;
+		// the nany sript to load, which should be the first new argument
+		const char* nargv0 = nargv[0];
+
+		--nargc;
+		uint32_t pargc = (nargc > 0) ? static_cast<uint32_t>(nargc) : 0;
+		const char** pargv = (!pargc ? nullptr : (++nargv));
+
+		exitstatus = nyrun_file_n(&runcf, nargv0, strlen(nargv0), pargc, pargv);
+	}
+
+	nyrun_cf_release(&runcf);
+	return exitstatus;
 }
