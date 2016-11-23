a29a5115c43caceaf7c0031adece7fb02964ab17
Damien GERARD <damien@iwi.me>
2016-11-23 22:19:04 +0900

    misc: coding style unittest

--- bootstrap/src/unittest.cpp
+++ bootstrap/src/unittest.cpp
@@ -33,12 +33,14 @@ struct Settings final {
 	colors;
 };
 
+
 void shuffleUnittests(std::vector<String>& unittests) {
 	std::cout << "shuffling the tests...\n";
 	auto seed = std::chrono::system_clock::now().time_since_epoch().count();
 	auto useed = static_cast<uint32_t>(seed);
 	std::shuffle(unittests.begin(), unittests.end(), std::default_random_engine(useed));
 }
+
 
 bool parseCommandLine(Settings& settings, int argc, char** argv) {
 	GetOpt::Parser options;
@@ -72,6 +74,7 @@ bool parseCommandLine(Settings& settings, int argc, char** argv) {
 	return true;
 }
 
+
 void fetchUnittestList(nyrun_cf_t& runcf, std::vector<String>& torun, const char** filelist, uint32_t count) {
 	std::cout << "searching for unittests in all source files...\n" << std::flush;
 	runcf.build.entrypoint.size = 0; // disable any compilation by default
@@ -80,7 +83,8 @@ void fetchUnittestList(nyrun_cf_t& runcf, std::vector<String>& torun, const char
 	runcf.program.entrypoint.c_str = nullptr;
 	runcf.build.ignore_atoms = nytrue;
 	runcf.build.userdata = &torun;
-	runcf.build.on_unittest = [](void* userdata, const char* mod, uint32_t modlen, const char* name, uint32_t nlen) {
+	runcf.build.on_unittest = [](void* userdata, const char* mod, uint32_t modlen, const char* name,
+	uint32_t nlen) {
 		AnyString module{mod, modlen};
 		AnyString testname{name, nlen};
 		((std::vector<String>*) userdata)->emplace_back();
@@ -91,12 +95,19 @@ void fetchUnittestList(nyrun_cf_t& runcf, std::vector<String>& torun, const char
 	nyrun_filelist(&runcf, filelist, count, 0, nullptr);
 	int64_t duration = DateTime::NowMilliSeconds() - starttime;
 	switch (torun.size()) {
-		case 0: std::cout << "0 test found"; break;
-		case 1: std::cout << "1 test found"; break;
-		default: std::cout << torun.size() << " tests found"; break;
+		case 0:
+			std::cout << "0 test found";
+			break;
+		case 1:
+			std::cout << "1 test found";
+			break;
+		default:
+			std::cout << torun.size() << " tests found";
+			break;
 	}
 	std::cout << " (" << duration << "ms)\n";
 }
+
 
 int listAllUnittests(nyrun_cf_t& runcf, const Settings& settings, const char** filelist, uint32_t count) {
 	std::set<std::pair<String, String>> alltests;
@@ -106,27 +117,24 @@ int listAllUnittests(nyrun_cf_t& runcf, const Settings& settings, const char** f
 	runcf.program.entrypoint.c_str = nullptr;
 	runcf.build.ignore_atoms = nytrue;
 	runcf.build.userdata = &alltests;
-	runcf.build.on_unittest = [](void* userdata, const char* mod, uint32_t modlen, const char* name, uint32_t nlen)
-	{
+	runcf.build.on_unittest = [](void* userdata, const char* mod, uint32_t modlen, const char* name,
+	uint32_t nlen) {
 		AnyString module{mod, modlen};
 		AnyString testname{name, nlen};
 		auto& dict = *((std::set<std::pair<String, String>>*) userdata);
 		dict.emplace(std::make_pair(String{module}, String{testname}));
 	};
 	int64_t starttime = DateTime::NowMilliSeconds();
-	if (0 != nyrun_filelist(&runcf, filelist, count, 0, nullptr))
-	{
+	if (0 != nyrun_filelist(&runcf, filelist, count, 0, nullptr)) {
 		std::cerr << "error: failed to compile. aborting.\n";
 		return EXIT_FAILURE;
 	}
 	int64_t duration = DateTime::NowMilliSeconds() - starttime;
 	AnyString lastmodule;
 	uint32_t moduleCount = 0;
-	for (auto& pair: alltests)
-	{
+	for (auto& pair : alltests) {
 		auto& module = pair.first;
-		if (lastmodule != module)
-		{
+		if (lastmodule != module) {
 			++moduleCount;
 			lastmodule = module;
 			if (settings.colors.out)
@@ -144,14 +152,25 @@ int listAllUnittests(nyrun_cf_t& runcf, const Settings& settings, const char** f
 	if (settings.colors.out)
 		System::Console::SetTextColor(std::cout, System::Console::lightblue);
 	switch (moduleCount) {
-		case 0:  break;
-		case 1:  std::cout << "1 module, "; break;
-		default: std::cout << moduleCount << " modules, "; break;
+		case 0:
+			break;
+		case 1:
+			std::cout << "1 module, ";
+			break;
+		default:
+			std::cout << moduleCount << " modules, ";
+			break;
 	}
 	switch (alltests.size()) {
-		case 0:  std::cout << "0 test found"; break;
-		case 1:  std::cout << "1 test found"; break;
-		default: std::cout << alltests.size() << " tests found"; break;
+		case 0:
+			std::cout << "0 test found";
+			break;
+		case 1:
+			std::cout << "1 test found";
+			break;
+		default:
+			std::cout << alltests.size() << " tests found";
+			break;
 	}
 	if (settings.colors.out)
 		System::Console::ResetTextColor(std::cout);
@@ -159,8 +178,10 @@ int listAllUnittests(nyrun_cf_t& runcf, const Settings& settings, const char** f
 	return EXIT_SUCCESS;
 }
 
+
 template<bool Fancy>
-bool runtest(nyrun_cf_t& originalRuncf, const Settings& settings, const String& testname, const char** filelist, uint32_t count) {
+bool runtest(nyrun_cf_t& originalRuncf, const Settings& settings, const String& testname,
+		 const char** filelist, uint32_t count) {
 	ShortString256 entry;
 	entry << "^unittest^" << testname;
 	entry.replace("<nomodule>", "module");
@@ -177,15 +198,15 @@ bool runtest(nyrun_cf_t& originalRuncf, const Settings& settings, const String& 
 	runcf.console.internal = &console;
 	runcf.console.flush = [](void*, nyconsole_output_t) {};
 	runcf.console.set_color = [](void*, nyconsole_output_t, nycolor_t) {};
-	runcf.console.has_color = [](void*, nyconsole_output_t) { return nyfalse; };
-	runcf.console.write_stdout = [](void* userdata, const char* text, size_t length)
-	{
+	runcf.console.has_color = [](void*, nyconsole_output_t) {
+		return nyfalse;
+	};
+	runcf.console.write_stdout = [](void* userdata, const char* text, size_t length) {
 		auto& console = *((Console*) userdata);
 		if (text and length)
 			console.cout.append(text, static_cast<uint32_t>(length));
 	};
-	runcf.console.write_stderr = [](void* userdata, const char* text, size_t length)
-	{
+	runcf.console.write_stderr = [](void* userdata, const char* text, size_t length) {
 		auto& console = *((Console*) userdata);
 		if (text and length)
 			console.cerr.append(text, static_cast<uint32_t>(length));
@@ -222,9 +243,8 @@ bool runtest(nyrun_cf_t& originalRuncf, const Settings& settings, const String& 
 		if (settings.colors.out)
 			System::Console::ResetTextColor(std::cout);
 		std::cout << testname;
-		if (duration < 1200 or not settings.colors.out) {
+		if (duration < 1200 or not settings.colors.out)
 			std::cout << "  (" << duration << "ms)     ";
-		}
 		else {
 			System::Console::SetTextColor(std::cout, System::Console::purple);
 			std::cout << "  (" << duration << "ms)     ";
@@ -241,30 +261,47 @@ bool runtest(nyrun_cf_t& originalRuncf, const Settings& settings, const String& 
 	return success;
 }
 
+
 void printStatstics(const Settings& settings, int64_t duration, uint32_t successCount, uint32_t failCount) {
 	switch (settings.unittests.size()) {
-		case 0:  std::cout << "\n       0 test, "; break;
-		case 1:  std::cout << "\n       1 test"; break;
-		default: std::cout << "\n       " << settings.unittests.size() << " tests, "; break;
+		case 0:
+			std::cout << "\n       0 test, ";
+			break;
+		case 1:
+			std::cout << "\n       1 test";
+			break;
+		default:
+			std::cout << "\n       " << settings.unittests.size() << " tests, ";
+			break;
 	}
 	if (successCount and settings.colors.out)
 		System::Console::SetTextColor(std::cout, System::Console::green);
 	switch (successCount) {
-		case 0:  std::cout << "0 passing"; break;
-		case 1:  std::cout << "1 passing"; break;
-		default: std::cout << successCount << " passing"; break;
+		case 0:
+			std::cout << "0 passing";
+			break;
+		case 1:
+			std::cout << "1 passing";
+			break;
+		default:
+			std::cout << successCount << " passing";
+			break;
 	}
 	if (successCount and settings.colors.out)
 		System::Console::ResetTextColor(std::cout);
 	if (failCount)
 		std::cout << ", ";
-
 	if (failCount and settings.colors.out)
 		System::Console::SetTextColor(std::cout, System::Console::red);
 	switch (failCount) {
-		case 0:  break;
-		case 1:  std::cout << "1 failed"; break;
-		default: std::cout << "" << failCount << " failed"; break;
+		case 0:
+			break;
+		case 1:
+			std::cout << "1 failed";
+			break;
+		default:
+			std::cout << "" << failCount << " failed";
+			break;
 	}
 	if (failCount and settings.colors.out)
 		System::Console::ResetTextColor(std::cout);
@@ -276,6 +313,7 @@ void printStatstics(const Settings& settings, int64_t duration, uint32_t success
 	std::cout << "\n\n";
 }
 
+
 bool runUnittests(nyrun_cf_t& runcf, const Settings& settings, const char** filelist, uint32_t filecount) {
 	if (settings.unittests.empty())
 		throw std::runtime_error("error: no unit test name");
@@ -285,7 +323,7 @@ bool runUnittests(nyrun_cf_t& runcf, const Settings& settings, const char** file
 	uint32_t successCount = 0;
 	uint32_t failCount = 0;
 	int64_t starttime = DateTime::NowMilliSeconds();
-	for (auto& testname: settings.unittests) {
+	for (auto& testname : settings.unittests) {
 		bool localsuccess = runtest<true>(runcf, settings, testname, filelist, filecount);
 		++(localsuccess ? successCount : failCount);
 	}
@@ -294,17 +332,19 @@ bool runUnittests(nyrun_cf_t& runcf, const Settings& settings, const char** file
 	return (failCount == 0 and successCount != 0);
 }
 
+
 auto canonicalizeAllFilenames(std::vector<String>& remainingArgs) {
-	auto filelist = std::make_unique<const char*[]>(remainingArgs.size());
+	auto filelist = std::make_unique<const char* []>(remainingArgs.size());
 	String filename;
 	size_t i = 0;
-	for (auto& arg: remainingArgs) {
+	for (auto& arg : remainingArgs) {
 		IO::Canonicalize(filename, arg);
 		arg = filename;
 		filelist[i++] = arg.c_str();
 	}
 	return filelist;
 }
+
 
 int runAllUnittests(nyrun_cf_t& runcf, Settings& settings, const char** filelist, uint32_t filecount) {
 	if (settings.colors.out)
@@ -323,12 +363,10 @@ int runAllUnittests(nyrun_cf_t& runcf, Settings& settings, const char** filelist
 	// no unittest provided from the command - default: all
 	if (settings.unittests.empty())
 		fetchUnittestList(runcf, settings.unittests, filelist, filecount);
-
 	int exitcode = EXIT_SUCCESS;
 	for (uint32_t r = 0; r != settings.repeat; ++r) {
 		if (settings.shuffle)
 			shuffleUnittests(settings.unittests);
-
 		if (settings.shuffle or settings.repeat > 1) {
 			std::cout << "running " << settings.unittests.size() << " tests...";
 			if (settings.repeat > 1) {
@@ -352,8 +390,7 @@ int runAllUnittests(nyrun_cf_t& runcf, Settings& settings, const char** filelist
 } // anonymous
 
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 	try {
 		Settings settings;
 		if (not parseCommandLine(settings, argc, argv))
@@ -362,14 +399,13 @@ int main(int argc, char** argv)
 		auto filelist = canonicalizeAllFilenames(settings.remainingArgs);
 		settings.colors.out = System::Console::IsStdoutTTY();
 		settings.colors.err = System::Console::IsStderrTTY();
-
 		nyrun_cf_t runcf;
 		nyrun_cf_init(&runcf);
 		if (settings.nslTests)
 			runcf.project.with_nsl_unittests = nytrue;
 		return (settings.listAll)
-			? listAllUnittests(runcf, settings, filelist.get(), filecount)
-			: runAllUnittests(runcf, settings, filelist.get(), filecount);
+			   ? listAllUnittests(runcf, settings, filelist.get(), filecount)
+			   : runAllUnittests(runcf, settings, filelist.get(), filecount);
 	}
 	catch (const std::exception& e) {
 		std::cerr << argv[0] << ": " << e.what() << '\n';
