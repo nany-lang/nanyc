56c4fb2e3cb4bbcb8ad0290df0838aca25407018
Damien GERARD <damien@iwi.me>
2016-11-23 22:14:54 +0900

    misc: coding style pass src2ast

--- bootstrap/libnanyc/details/pass/a-src2ast/ast-from-source.cpp
+++ bootstrap/libnanyc/details/pass/a-src2ast/ast-from-source.cpp
@@ -2,24 +2,19 @@
 #include "details/context/build-info.h"
 
 
+namespace ny {
 
-namespace ny
-{
 
-	bool Source::passASTFromSourceWL()
-	{
-		auto& parser = pBuildInfo->parsing.parser;
-		switch (m_type)
-		{
-			case Type::file:
-				return parser.loadFromFile(m_filename) and parser.root;
-
-			case Type::memory:
-				return parser.load(m_content) and parser.root;
-		}
-
-		return false;
+bool Source::passASTFromSourceWL() {
+	auto& parser = pBuildInfo->parsing.parser;
+	switch (m_type) {
+		case Type::file:
+			return parser.loadFromFile(m_filename) and parser.root;
+		case Type::memory:
+			return parser.load(m_content) and parser.root;
 	}
+	return false;
+}
 
 
 } // namespace ny
