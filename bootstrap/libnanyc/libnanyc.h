5f5c3b461078b8f8db88284262545ba8529a9804
Damien GERARD <damien@iwi.me>
2016-11-23 18:00:10 +0900

    misc: add comment for ignoring C4251

--- bootstrap/libnanyc/libnanyc.h
+++ bootstrap/libnanyc/libnanyc.h
@@ -2,7 +2,7 @@
 #include <yuni/yuni.h>
 
 #ifdef YUNI_OS_MSVC
-#pragma warning(disable: 4251)
+#pragma warning(disable: 4251) // class 'type' needs to have dll-interface
 #endif
 
 #define likely(X)    YUNI_LIKELY(X)
