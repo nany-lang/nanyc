4a2bbb888c134dd4f0b9b1cef91a0df7df1053a2
Damien Gerard <damien@iwi.me>
2016-12-17 00:08:46 +0900

    cmake: utility func 'system' to easily get output of shell command

--- bootstrap/cmake/utils.cmake
+++ bootstrap/cmake/utils.cmake
@@ -185,4 +185,7 @@ macro(string_split var delimiter str)
 endmacro()
 
 
-
+macro(system varname)
+	execute_process(COMMAND ${ARGN} OUTPUT_VARIABLE ${varname}
+		OUTPUT_STRIP_TRAILING_WHITESPACE)
+endmacro()
