323dde20d35b1d4fb451a8c244e12b886a296aa9
Damien Gerard <damien@iwi.me>
2016-12-17 15:02:58 +0900

    travis: use terminal output folding

--- assets/build/build.sh
+++ assets/build/build.sh
@@ -18,8 +18,20 @@ die() {
 }
 
 run() {
-	title $1; shift
-	$@
+	local tag="$1"
+	local title="$2"
+	local ANSI_CLEAR="\033[0K"
+	shift
+	shift
+	if [ -n "${TRAVIS_JOB_NUMBER:-}" ]; then
+		echo -en "travis_fold:start:${tag}\r${ANSI_CLEAR}"
+		echo "${title}"
+	else
+		title "${title}"
+	fi
+	$@ || die "failed to execute '$@'"
+	[ -n "${TRAVIS_JOB_NUMBER:-}" ] && echo -en "travis_fold:end:${tag}\r${ANSI_CLEAR}"
+	return 0
 }
 
 platform_and_env_settings() {
@@ -69,7 +81,7 @@ configure() {
 
 build() {
 	echo "# make -j ${NPROC}"
-	make -j ${NPROC} || (run  "SOMETHING HAS FAILED" make VERBOSE=1 ; exit 1) || die "build failed"
+	make -j ${NPROC} || (run  "retry" "ERROR" make VERBOSE=1 ; exit 1) || die "build failed"
 	make check || die "check failed"
 }
 
@@ -85,8 +97,8 @@ print_packages() {
 make_packages() {
 	[ ! "${BUILD_TYPE:-}" = 'release' ] && return
 	local has_pkgs=0
-	[ $platform == linux ] && run "Packages DEB" make_packages_deb && has_pkgs=1
-	[ $has_pkgs -ne 0 ] && run "Distribution" print_packages
+	[ $platform == linux ] && run "DEB" "Packages DEB" make_packages_deb && has_pkgs=1
+	[ $has_pkgs -ne 0 ] && run "output" "Distribution" print_packages
 	return 0
 }
 
@@ -95,10 +107,10 @@ local_pwd=$(dirname "$0")
 root=$(cd "${local_pwd}" && pwd)
 pushd "${root}/../../bootstrap"
 
-run "Platform & Env settings" platform_and_env_settings
-run "C++ Compiler settings" compiler_settings
+run "settings" "Platform / Env Settings" platform_and_env_settings
+run "compiler" "C++ Compiler settings" compiler_settings
 [ -z "${TRAVIS_JOB_NUMBER}" ] && cmake_cleanup
 
-run "Bootstrap: configure" configure
-run "Bootstrap: build" build
+run "cmake" "Bootstrap: configure" configure
+run "build" "Bootstrap: build" build
 make_packages
