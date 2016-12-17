405cebfde1770b7b0bf940aafd18b13edcf00cd5
Damien Gerard <damien@iwi.me>
2016-12-17 14:44:06 +0900

    travis-ci: move each part of the build into its own function

--- assets/build/build.sh
+++ assets/build/build.sh
@@ -17,70 +17,90 @@ die() {
 	exit 1
 }
 
+platform_and_env_settings() {
+	title "GENERAL"
+	platform='unknown'
+	unamestr=`uname`
+	if [[ "$unamestr" == 'Linux' ]]; then
+		platform='linux'
+	elif [[ "$unamestr" == 'FreeBSD' ]]; then
+		platform='freebsd'
+	elif [[ "$unamestr" == 'Darwin' ]]; then
+		platform='macos'
+	fi
+	echo "platform: ${platform}"
+
+	export NPROC=1
+	[ $platform == linux ] && export NPROC=$(nproc)
+	[ $platform == macos ] && export NPROC=$(sysctl -n hw.ncpu)
+	export platform="${platform}"
+	[ "$BUILD_TYPE" == "" ] && export BUILD_TYPE="debug"
+
+	uname -a
+	cat	./build-settings.txt
+}
+
+compiler_settings() {
+	if [ -n "${TRAVIS_OS_NAME}" -a $platform == linux ]; then
+		[ "$CC"  == "" ]    && export CC="gcc-4.9";
+		[ "$CXX" == "" ]    && export CXX="g++-4.9";
+		[ "$CC"  == "gcc" ] && export CC="gcc-4.9";
+		[ "$CXX" == "g++" ] && export CXX="g++-4.9";
+	fi
+	[ -z "$CC" ]  && export CC="gcc";
+	[ -z "$CXX" ] && export CXX="g++";
+}
+
+cmake_cleanup() {
+	title "CLEANUP"
+	cd bootstrap
+	echo " - delete CMakeCache.txt" && rm -f CMakeCache.txt
+	echo " - delete CMakeFiles" && rm -rf CMakeFiles
+}
+
+configure() {
+	title "CONFIGURE"
+	echo "# cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
+	cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} || die "bootstrap configure error"
+}
+
+build() {
+	title "BUILD"
+	echo "# make -j ${NPROC}"
+	make -j ${NPROC} || (title "MAKE ERROR"; make VERBOSE=1 ; exit 1) || die "build failed"
+	make check || die "check failed"
+}
+
+make_packages_deb() {
+	title "PACKAGES DEB"
+	make packages-deb || die "packages deb failed";
+}
+
+print_packages() {
+	title "FOR DISTRIBUTION"
+	(cd "${root}/../../distrib" && find  -maxdepth 1 -and -type f  -and -not -path '*/\.*' \
+		| xargs  ls -lh)
+}
+
+make_packages() {
+	if [ ! "${BUILD_TYPE:-}" = 'release' ]; then
+		return
+	fi
+	local has_pkgs=0
+	[ $platform == linux ] && make_packages_deb && has_pkgs=1
+	[ $has_pkgs -ne 0 ] && print_packages
+	return 0
+}
+
+
 local_pwd=$(dirname "$0")
 root=$(cd "${local_pwd}" && pwd)
 pushd "${root}/../../"
 
+platform_and_env_settings
+compiler_settings
+[ -z "${TRAVIS_JOB_NUMBER}" ] && cmake_cleanup
 
-title "GENERAL"
-platform='unknown'
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-	platform='linux'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-	platform='freebsd'
-elif [[ "$unamestr" == 'Darwin' ]]; then
-	platform='macos'
-fi
-echo "platform: ${platform}"
-
-export NPROC=1
-[ $platform == linux ] && export NPROC=$(nproc)
-[ $platform == macos ] && export NPROC=$(sysctl -n hw.ncpu)
-
-[ "$BUILD_TYPE" == "" ] && export BUILD_TYPE="debug"
-
-
-
-# update on TRAVIS
-if [ -n "${TRAVIS_OS_NAME}" -a $platform == linux ]; then
-	[ "$CC"  == "" ]    && export CC="gcc-4.9";
-	[ "$CXX" == "" ]    && export CXX="g++-4.9";
-	[ "$CC"  == "gcc" ] && export CC="gcc-4.9";
-	[ "$CXX" == "g++" ] && export CXX="g++-4.9";
-fi
-[ -z "$CC" ]  && export CC="gcc";
-[ -z "$CXX" ] && export CXX="g++";
-
-
-title "ENVIRONMENT"
-uname -a
-cat ./build-settings.txt
-echo
-
-
-title "CLEANUP"
-cd bootstrap
-echo " - delete CMakeCache.txt" && rm -f CMakeCache.txt
-echo " - delete CMakeFiles" && rm -rf CMakeFiles
-
-
-title "CONFIGURE"
-echo "# cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
-cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} || die "bootstrap configure error"
-
-
-title "BUILD"
-echo "# make -j ${NPROC}"
-make -j ${NPROC} || (title "MAKE ERROR"; make VERBOSE=1 ; exit 1) || die "build failed"
-make check || die "check failed"
-
-
-if [ $platform == linux -a \( -z "${TRAVIS_JOB_NUMBER}" -o "${BUILD_TYPE:-}" = 'release' \) ]; then
-	title "PACKAGE DEB"
-	make packages-deb || die "packages deb failed";
-fi
-
-
-title "OUTPUT"
-ls -lh "${root}/../../distrib"
+configure
+build
+make_packages
