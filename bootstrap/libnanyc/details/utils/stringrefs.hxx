0bb5493149ce526d23d705f34f6208ce1395a7e8
Damien Gerard <damien@iwi.me>
2017-01-04 21:32:56 +0900

    stringrefs: style: rename attributes

--- bootstrap/libnanyc/details/utils/stringrefs.hxx
+++ bootstrap/libnanyc/details/utils/stringrefs.hxx
@@ -6,39 +6,39 @@ namespace ny {
 
 
 inline StringRefs::StringImmutablePointer::~StringImmutablePointer() {
-	delete[] text;
+	delete[] m_cstr;
 }
 
 
 inline StringRefs::StringImmutablePointer::StringImmutablePointer(const AnyString& string)
-	: text(new char[string.size() + 1])
-	, size(string.size()) {
-	::memcpy(text, string.c_str(), sizeof(char) * size);
-	text[size] = '\0';
+	: m_cstr(new char[string.size() + 1])
+	, m_size(string.size()) {
+	::memcpy(m_cstr, string.c_str(), sizeof(char) * m_size);
+	m_cstr[m_size] = '\0';
 }
 
 
 inline StringRefs::StringImmutablePointer::StringImmutablePointer(StringRefs::StringImmutablePointer&& rhs)
-	: text(rhs.text)
-	, size(rhs.size) {
-	rhs.text = nullptr;
+	: m_cstr(rhs.m_cstr)
+	, m_size(rhs.m_size) {
+	rhs.m_cstr = nullptr;
 }
 
 
 inline AnyString StringRefs::StringImmutablePointer::toString() const {
-	return AnyString{text, size};
+	return AnyString{m_cstr, m_size};
 }
 
 
 inline bool StringRefs::exists(const AnyString& text) const {
-	return (pIndex.count(text) != 0);
+	return (m_index.count(text) != 0);
 }
 
 
 inline uint32_t StringRefs::ref(const AnyString& text) {
 	if (YUNI_LIKELY(not text.empty())) {
-		auto it = pIndex.find(text);
-		return (it != pIndex.end()) ? it->second : keepString(text);
+		auto it = m_index.find(text);
+		return (it != m_index.end()) ? it->second : keepString(text);
 	}
 	return 0;
 }
@@ -49,8 +49,8 @@ inline AnyString StringRefs::refstr(const AnyString& text) {
 
 
 inline AnyString StringRefs::operator [] (uint32_t ix) const {
-	assert(ix < pRefs.size());
-	return pRefs[ix].toString();
+	assert(ix < m_storage.size());
+	return m_storage[ix].toString();
 }
 
 
