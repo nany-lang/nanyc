954ce0a5a7b101a0e412857a375fa2e464e63506
Damien Gerard <damien@iwi.me>
2017-01-22 20:07:39 +0900

    semantic: remove empty lines

--- bootstrap/libnanyc/details/semantic/atom-factory.h
+++ bootstrap/libnanyc/details/semantic/atom-factory.h
@@ -30,7 +30,6 @@ struct InstanciateData final {
 
 	//! The original view to the classdef table
 	ClassdefTableView& cdeftable;
-
 	//! The atom to instanciate
 	std::reference_wrapper<Atom> atom;
 	//! The parent atom, if any
@@ -39,31 +38,25 @@ struct InstanciateData final {
 	uint32_t instanceid = (uint32_t) - 1;
 	//! Context
 	Build& build;
-
 	//! Parameters used for instanciation
 	decltype(FuncOverloadMatch::result.params)& params;
 	//! Template parameters
 	decltype(FuncOverloadMatch::result.params)& tmplparams;
 	//!
 	Classdef returnType;
-
 	//! Flag to determine whether the code can be generated or not
 	bool canGenerateCode = true;
 	//! Flag to determine whether the code generate errors or not
 	bool canGenerateErrors = true;
-
 	//! Make the layer persistent
 	bool shouldMergeLayer = false;
-
 	//! Parent
 	SequenceBuilder* parent = nullptr;
-
 	//! Error reporting
 	std::shared_ptr<Logs::Message>& report;
-
 	bool signatureOnly = false;
 
-}; // class InstanciateData
+}; // struct InstanciateData
 
 
 /*!
