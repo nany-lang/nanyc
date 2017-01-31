31e1aac987e9f79725e145cc3e991250ab83fc72
Damien Gerard <damien@iwi.me>
2017-01-31 21:23:54 +0900

    ast2ir: add exceptions

---
+++ bootstrap/libnanyc/details/pass/c-ast2ir/exception.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <yuni/core/string.h>
+#include "details/grammar/nany.h"
+#include <exception>
+
+
+namespace ny {
+namespace ir {
+namespace Producer {
+
+
+struct Error: public std::exception {
+	Error(const AST::Node& node, const char* = "");
+	const char* what() const noexcept override { return ""; }
+	const AST::Node& node;
+	const yuni::String message;
+};
+
+
+struct AttributeError final: public Error {
+	AttributeError(const AST::Node& node, const yuni::ShortString32&, const char* = "");
+	yuni::ShortString32 attrname;
+};
+
+
+struct UnexpectedNode final: public Error {
+	using Error::Error;
+};
+
+
+} // namespace Producer
+} // namespace ir
+} // namespace ny
