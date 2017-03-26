3583e107e2283849ebd58d3768ef777c59bd9886
Damien Gerard <damien@iwi.me>
2017-03-26 22:05:05 +0900

    ast2ir: update opcodes comments

--- bootstrap/libnanyc/details/ir/isa/opcodes.h
+++ bootstrap/libnanyc/details/ir/isa/opcodes.h
@@ -21,190 +21,101 @@ enum class Op : std::uint32_t {
 	//! nop opcode (or unknown)
 	nop = 0,
 
-	//! and
-	opand,
-	//! or
-	opor,
-	//! xor
-	opxor,
-	//! mod
-	opmod,
-	//! equal
-	eq,
-	//! not equal
-	neq,
-	//! less than
-	lt,
-	//! less than (signed)
-	ilt,
-	//! less than or equal
-	lte,
-	//! less than or equal (signed)
-	ilte,
-	//! greater than
-	gt,
-	//! greater than (signed)
-	igt,
-	//! greater than or equal
-	gte,
-	//! greaterthan or equal (signed)
-	igte,
-	//! less than (floating point bumber)
-	flt,
-	//! less than or equal (floating point bumber)
-	flte,
-	//! greater than (floating point bumber)
-	fgt,
-	//! greater than or equal (floating point bumber)
-	fgte,
+	opand,          ///< and
+	opor,           ///< or
+	opxor,          ///< xor
+	opmod,          ///< mod
+	eq,             ///< equal
+	neq,            ///< not equal
+	lt,             ///< less than
+	ilt,            ///< less than (signed)
+	lte,            ///< less than or equal
+	ilte,           ///< less than or equal (signed)
+	gt,             ///< greater than
+	igt,            ///< greater than (signed)
+	gte,            ///< greater than or equal
+	igte,           ///< greaterthan or equal (signed)
+	flt,            ///< less than (floating point number)
+	flte,           ///< less than or equal (floating point number)
+	fgt,            ///< greater than (floating point number)
+	fgte,           ///< greater than or equal (floating point number)
 
-	//! not
-	negation,
+	negation,       ///< not
 
-	//! +
-	add,
-	//! -
-	sub,
-	//! *
-	mul,
-	//! /
-	div,
-	//! * (signed)
-	imul,
-	//! / (signed)
-	idiv,
+	add,            ///< +
+	sub,            ///< -
+	mul,            ///< *
+	div,            ///< /
+	imul,           ///< * (signed)
+	idiv,           ///< / (signed)
+	fadd,           ///< + (floating point number)
+	fsub,           ///< - (floating point number)
+	fmul,           ///< * (floating point number)
+	fdiv,           ///< / (floating point number)
 
-	//! +
-	fadd,
-	//! -
-	fsub,
-	//! *
-	fmul,
-	//! /
-	fdiv,
+	storeConstant,  ///< set a local variable
+	store,          ///< set a local variable
+	storeText,      ///< string literal
+	stackalloc,     ///< allocate a local variable on the stack
 
+	jmp,            ///< unconditional jump
+	jz,             ///< jump if local variable is zero
+	jnz,            ///< jump if local variable is not zero
 
-	//! Store a constant value in a register
-	storeConstant,
-	//! Store value from another register
-	store,
-	//! Store a context text
-	storeText,
-	//! alloca (on the stack)
-	stackalloc,
+	ref,            ///< increment the reference count
+	unref,          ///< decrement the reference count (release it if reaches 0)
+	push,           ///< push indexed or named parameter for next function call
+	call,           ///< function call
+	intrinsic,      ///< compiler intrinsic call
+	ret,            ///< return
+	allocate,       ///< allocate an object (memory + ctor)
+	dispose,        ///< deallocate an object (dtor + memory)
 
-	//! unconditional jump
-	jmp,
-	//! jump if local variable is zero
-	jz,
-	//! jump if local variable is not zero
-	jnz,
+	stacksize,      ///< define the stack size for the current blueprint
+	pragma,         ///< metadata for blueprints
 
+	load_u64,       ///< read a 64 bits value from memory
+	load_u32,       ///< read a 32 bits value from memory
+	load_u8,        ///< read a 8 bits value from memory
+	store_u64,      ///< write a 64 bits value in memory
+	store_u32,      ///< write a 32 bits value in memory
+	store_u8,       ///< write a 8b its value in memory
 
-	//! acquire object
-	ref,
-	//! release object
-	unref,
-	//! push indexed parameter
-	push,
-	//! call a function
-	call,
-	//! call an intrinsic function
-	intrinsic,
-	//! return value
-	ret,
-	//! Allocate object
-	allocate,
-	//! Release an object (no ref counting)
-	dispose,
-
-	//! Increase stack size
-	stacksize,
-	//! pragma
-	pragma,
-
-	//! Read 64bits from memory
-	load_u64,
-	//! Read 32bits from emmory
-	load_u32,
-	//! Read 8bits from emmory
-	load_u8,
-	//! Write 64bits into memory
-	store_u64,
-	//! Write 32bits into memory
-	store_u32,
-	//! Write 8bits into memory
-	store_u8,
-	//! Allocate a region of memory
-	memalloc,
-	//! Free a region of memory previously allocated by 'memalloc'
-	memfree,
-	//! Reallocate a pointer
-	memrealloc,
-	//! Fill a memory with a pattern
-	memfill,
-	//! Memcopy
-	memcopy,
-	//! Memcopy
-	memmove,
-	//! Memcmp
-	memcmp,
-	//! strlen (32/64)
-	cstrlen,
-
-	//! label
-	label,
-
-	//! Assert expression
-	opassert,
-	//! Register a valid pointer (used for MemChecker)
-	memcheckhold,
+	memalloc,       ///< allocate a chunk of memory
+	memfree,        ///< release a previously allocated chunk of memory
+	memrealloc,     ///< reallocate a memory area
+	memfill,        ///< fill a memory area with a pattern
+	memcopy,        ///< see memcpy
+	memmove,        ///< see memmove
+	memcmp,         ///< see memcmp
+	cstrlen,        ///< see strlen (32 or 64bits)
+	label,          ///< define a new label
+	opassert,       ///< assert if expr is false
+	memcheckhold,   ///< mark a pointer as valid (MemChecker)
 
 
 	// --- opcodes for compilation only
+	identify,       ///< resolve an identifier completely or partially (if overload)
+	identifyset,    ///< like 'identify', but perfers setters instead of getters
+	ensureresolved, ///< resolve completely type of expr (if not done by 'identify')
+	commontype,     ///< determine common type
+	assign,         ///< assign a variable to another
+	self,           ///< declare a register as 'self'
+	tpush,          ///< push a generic type parameter
+	follow,         ///< follow
+	blueprint,      ///< define a new blueprint (class, func...)
+	classdefsizeof, ///< compute the size of a type
+	fieldget,       ///< read the value of a variable member
+	fieldset,       ///< set the value of a variable member
+	debugfile,      ///< current source file
+	debugpos,       ///< current position in the current file
+	namealias,      ///< variable name
+	comment,        ///< just some comments
+	scope,          ///< begining of a new scope
+	typeisobject,   ///< check if a given type is an object
+	qualifiers,     ///< ref, conf
 
-	//! try to identity an identifier (partially or completely from its referer)
-	identify,
-	//! try to identity an id setter if ambigous
-	// (this opcode is strictly identical to 'identify')
-	identifyset,
-	//! try to identify completely the type of sub expression
-	// (if not already done by 'identity')
-	ensureresolved,
-	//! common type
-	commontype,
-	//! assign a variable to another
-	assign,
-	//! declare a register as 'self'
-	self,
-	//! push a generic type parameter
-	tpush,
-	//! follow
-	follow,
-	//! Blueprint
-	blueprint,
-	//! sizeof
-	classdefsizeof,
-	//! field read
-	fieldget,
-	//! field write
-	fieldset,
-	//! the current source file
-	debugfile,
-	//! the current position in the current source file
-	debugpos,
-	//! named alias
-	namealias,
-	//! Comment within the sequence
-	comment,
-	//! begining of a new scope
-	scope,
-	//! Check if a given type is an object
-	typeisobject,
-	//! Qualifiers
-	qualifiers,
-	//! end of a scope
-	end,
+	end,            ///< end of scope (and the last opcode)
 };
 
 
