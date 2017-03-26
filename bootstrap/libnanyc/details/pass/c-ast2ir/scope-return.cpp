d20dc03218561faa226c41414b0a1af424746332
Damien Gerard <damien@iwi.me>
2017-03-26 19:13:16 +0900

    ast2ir: return: use returned expression lvid instead of additional flag

--- bootstrap/libnanyc/details/pass/c-ast2ir/scope-return.cpp
+++ bootstrap/libnanyc/details/pass/c-ast2ir/scope-return.cpp
@@ -14,27 +14,25 @@ bool Scope::visitASTExprReturn(AST::Node& node) {
 	assert(node.rule == AST::rgReturn);
 	// assert(not node.children.empty()); -- a return may be empty
 	bool success = true;
-	bool hasReturnValue = false;
+	uint32_t retlvid = 0;
 	auto& irout = ircode();
 	for (auto& child : node.children) {
 		switch (child.rule) {
 			case AST::rgExpr: {
 				ir::emit::ScopeLocker opscope{irout};
-				uint32_t retlvid = 0;
 				success &= visitASTExpr(child, retlvid);
 				// generate error on the begining of the expr and not the return itself
 				emitDebugpos(child);
 				ir::emit::ret(irout, retlvid, ir::emit::alloc(irout, nextvar()));
-				hasReturnValue = true;
 				break;
 			}
 			default:
 				return unexpectedNode(child, "[ir/return]");
 		}
 	}
-	if (not hasReturnValue) {
+	if (retlvid == 0) {
 		emitDebugpos(node);
-		ir::emit::ret(irout);
+		ir::emit::ret(irout); // return void
 	}
 	return success;
 }
