c40240870ae95518152b467a39624d002492100b
Damien Gerard <damien@iwi.me>
2017-04-03 21:59:02 +0900

    cmake: use cmake-style arguments for system()

--- bootstrap/cmake/utils.cmake
+++ bootstrap/cmake/utils.cmake
@@ -1,4 +1,8 @@
 macro(system varname)
-	execute_process(COMMAND ${ARGN} OUTPUT_VARIABLE ${varname}
+	set(options)
+	set(oneValueArgs)
+	set(multiValueArgs COMMAND)
+	cmake_parse_arguments(opts "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+	execute_process(COMMAND ${opts_COMMAND} OUTPUT_VARIABLE ${varname}
 		OUTPUT_STRIP_TRAILING_WHITESPACE)
 endmacro()
