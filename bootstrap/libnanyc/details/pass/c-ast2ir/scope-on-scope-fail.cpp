aa8c18832a9a0bf363b3704e20f088c91a3a7881
Damien Gerard <damien@iwi.me>
2017-05-15 23:09:51 +0900

    on-scope-fail: destroy the error before jumping

--- bootstrap/libnanyc/details/pass/c-ast2ir/scope-on-scope-fail.cpp
+++ bootstrap/libnanyc/details/pass/c-ast2ir/scope-on-scope-fail.cpp
@@ -126,11 +126,11 @@ bool Scope::visitASTExprOnScopeFail(AST::Node& scopeNode, AST::Node& scopeFailNo
 	// register the 'on scope fail' after the code for the error handler to not be reused
 	emitDebugpos(scopeFailNode);
 	ir::emit::on::scopefail(irout, var, startLabel);
+	if (var != 0)
+		ir::emit::unref(irout, var, 0);
 	// jmp at the end of the original scope
 	uint32_t jmpOffset = ir::emit::jmp(irout);
 	onScopeFailExitLabels.emplace_back(scopeFailNode, jmpOffset, var);
-	if (var != 0)
-		ir::emit::unref(irout, var, 0);
 	// emit label to skip the entire block
 	irout.at<ir::isa::Op::jmp>(ignJmp).label = ir::emit::label(irout, nextvar());
 	ir::emit::trace(irout, "end 'on scope fail'");
