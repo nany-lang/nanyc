22cf7ca0a2873315e2196054817a2ffb35359286
Damien Gerard <damien@iwi.me>
2017-05-17 22:41:31 +0900

    c-api: add macros to export C functions

--- bootstrap/libnanyc/include/nanyc/types.h
+++ bootstrap/libnanyc/include/nanyc/types.h
@@ -15,6 +15,34 @@ extern "C" {
 #endif
 
 
+#if defined(_WIN32) || defined(__CYGWIN__)
+#  ifdef __GNUC__
+#    define LIBNANYC_VISIBILITY_EXPORT   __attribute__ ((dllexport))
+#    define LIBNANYC_VISIBILITY_IMPORT   __attribute__ ((dllimport))
+#  else
+#    define LIBNANYC_VISIBILITY_EXPORT   __declspec(dllexport) /* note: actually gcc seems to also supports this syntax */
+#    define LIBNANYC_VISIBILITY_IMPORT   __declspec(dllimport) /* note: actually gcc seems to also supports this syntax */
+#  endif
+#else
+#  define LIBNANYC_VISIBILITY_EXPORT     __attribute__((visibility("default")))
+#  define LIBNANYC_VISIBILITY_IMPORT     __attribute__((visibility("default")))
+#endif
+
+#if defined(_DLL) && !defined(LIBNANYC_DLL_EXPORT)
+#  define LIBNANYC_DLL_EXPORT
+#endif
+
+/*!
+** \macro NY_EXPORT
+** \brief Export / import a libnany symbol (function)
+*/
+#if defined(LIBNANYC_DLL_EXPORT)
+#   define NY_EXPORT LIBNANYC_VISIBILITY_EXPORT
+#else
+#   define NY_EXPORT LIBNANYC_VISIBILITY_IMPORT
+#endif
+
+
 /*! Boolean values */
 typedef enum nybool_t {
 	nyfalse = 0,
