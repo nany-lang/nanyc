8ed10e22d357a1e2c2b8e7ac226775c1bf4f35ff
Damien Gerard <damien@iwi.me>
2017-06-02 00:04:33 +0900

    compiler: import the list of input sources

--- bootstrap/libnanyc/details/compiler/compiler.cpp
+++ bootstrap/libnanyc/details/compiler/compiler.cpp
@@ -2,6 +2,8 @@
 #include "details/compiler/compiler.h"
 #include "details/program/program.h"
 #include "details/reporting/report.h"
+#include "details/errors/errors.h"
+#include <yuni/io/file.h>
 #include <libnanyc.h>
 #include <utility>
 #include <memory>
@@ -11,6 +13,8 @@ namespace compiler {
 
 namespace {
 
+constexpr uint32_t memoryHardlimit = 64 * 1024 * 1024;
+
 Logs::Report buildGenerateReport(void* ptr, Logs::Level level) {
 	return (*((ny::Logs::Report*) ptr)).fromErrLevel(level);
 }
@@ -18,6 +22,27 @@ Logs::Report buildGenerateReport(void* ptr, Logs::Level level) {
 nyprogram_t* complainNoSource(ny::Logs::Report& report) {
 	report.error() << "no input source code";
 	return nullptr;
+}
+
+void copySourceOpts(ny::compiler::Source& source, const nysource_opts_t& opts) {
+	if (opts.filename.len != 0) {
+		if (unlikely(opts.filename.len > memoryHardlimit))
+			throw "input filename bigger than internal limit";
+		yuni::IO::Canonicalize(source.filename, AnyString{opts.filename.c_str, static_cast<uint32_t>(opts.filename.len)});
+	}
+	if (opts.content.len != 0) {
+		if (unlikely(opts.content.len > memoryHardlimit))
+			throw "input source content bigger than internal limit";
+		source.content.assign(opts.content.c_str, static_cast<uint32_t>(opts.content.len));
+	}
+}
+
+void compileSource(ny::compiler::Source& source) {
+}
+
+void importSourceAndCompile(ny::compiler::Source& source, const nysource_opts_t& opts) {
+	copySourceOpts(source, opts);
+	compileSource(source);
 }
 
 } // namespace
@@ -29,8 +54,13 @@ inline Compiler::Compiler(const nycompile_opts_t& opts)
 inline nyprogram_t* Compiler::compile() {
 	ny::Logs::Report report{messages};
 	Logs::Handler errorHandler{&report, &buildGenerateReport};
-	if (unlikely(opts.sources.count == 0))
+	uint32_t scount = opts.sources.count;
+	if (unlikely(scount == 0))
 		return complainNoSource(report);
+	sources.count = scount;
+	sources.items = std::make_unique<Source[]>(scount);
+	for (uint32_t i = 0; i != opts.sources.count; ++i)
+		importSourceAndCompile(sources[i], opts.sources.items[i]);
 	auto program = std::make_unique<ny::Program>();
 	return ny::Program::pointer(program.release());
 }
--- bootstrap/libnanyc/details/compiler/compiler.h
+++ bootstrap/libnanyc/details/compiler/compiler.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <nanyc/program.h>
 #include "details/reporting/message.h"
+#include <memory>
+#include <cassert>
 
 namespace ny {
 namespace compiler {
+
+struct Source final {
+	yuni::String content;
+	yuni::String filename;
+};
 
 struct Compiler final {
 	Compiler(const nycompile_opts_t&);
@@ -11,6 +18,12 @@ struct Compiler final {
 
 	const nycompile_opts_t& opts;
 	Logs::Message messages{Logs::Level::none};
+	struct final {
+		uint32_t count = 0;
+		std::unique_ptr<Source[]> items;
+		Source& operator [] (uint32_t i) { assert(i < count); return items[i]; }
+	}
+	sources;
 };
 
 nyprogram_t* compile(nycompile_opts_t&);
