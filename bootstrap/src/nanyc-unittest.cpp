5a89c40ed8ccd2b0db565ecd2c7231ca3f2fb444
Damien Gerard <damien@iwi.me>
2017-06-08 23:50:14 +0900

    unittests: accept sanitized input filenames

--- bootstrap/src/nanyc-unittest.cpp
+++ bootstrap/src/nanyc-unittest.cpp
@@ -2,7 +2,12 @@
 #include <nanyc/program.h>
 #include <yuni/yuni.h>
 #include <yuni/core/getopt.h>
+#include <yuni/core/string.h>
+#include <yuni/io/filename-manipulation.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <memory>
 #include "libnanyc.h"
 
 
@@ -12,12 +17,37 @@ namespace {
 
 struct App final {
 	App();
+	App(App&&) = default;
+	~App();
+
+	void importFilenames(const std::vector<AnyString>&);
 
 	nycompile_opts_t opts;
+	std::vector<yuni::String> filenames;
 };
 
 App::App() {
 	memset(&opts, 0x0, sizeof(opts));
+}
+
+App::~App() {
+	free(opts.sources.items);
+}
+
+void App::importFilenames(const std::vector<AnyString>& list) {
+	uint32_t count = static_cast<uint32_t>(list.size());
+	filenames.resize(count);
+	std::transform(std::begin(list), std::end(list), std::begin(filenames), [](auto& item) -> yuni::String {
+		return std::move(yuni::IO::Canonicalize(item));
+	});
+	opts.sources.count = count;
+	opts.sources.items = (nysource_opts_t*) calloc(count, sizeof(nysource_opts_t));
+	if (unlikely(!opts.sources.items))
+		throw std::bad_alloc();
+	for (uint32_t i = 0; i != count; ++i) {
+		opts.sources.items[i].filename.len = filenames[i].size();
+		opts.sources.items[i].filename.c_str = filenames[i].c_str();
+	}
 }
 
 int printVersion() {
@@ -40,10 +70,13 @@ App prepare(int argc, char** argv) {
 	App app;
 	bool version = false;
 	bool bugreport = false;
+	std::vector<AnyString> filenames;
 	yuni::GetOpt::Parser options;
+	options.addFlag(filenames, 'i', "", "Input nanyc source files");
 	options.addParagraph("\nHelp");
 	options.addFlag(bugreport, 'b', "bugreport", "Display some useful information to report a bug");
 	options.addFlag(version, ' ', "version", "Print the version");
+	options.remainingArguments(filenames);
 	if (not options(argc, argv)) {
 		if (options.errors())
 			throw std::runtime_error("Abort due to error");
@@ -53,6 +86,7 @@ App prepare(int argc, char** argv) {
 		throw printVersion();
 	if (unlikely(bugreport))
 		throw printBugreport();
+	app.importFilenames(filenames);
 	return app;
 }
 
