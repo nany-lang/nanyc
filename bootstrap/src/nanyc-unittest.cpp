26cb4d2560bcf16cc5ed0ab77fafe48ae82aca51
Damien Gerard <damien@iwi.me>
2017-06-08 23:13:58 +0900

    unittests: start brand new shiny implementation from new C-API

---
+++ bootstrap/src/nanyc-unittest.cpp
@@ -0,0 +1,67 @@
+#include <nanyc/library.h>
+#include <yuni/yuni.h>
+#include <yuni/core/getopt.h>
+#include <iostream>
+#include "libnanyc.h"
+
+
+namespace ny {
+namespace unittests {
+namespace {
+
+struct App final {
+};
+
+int printVersion() {
+	std::cout << libnanyc_version_to_cstr() << '\n';
+	return EXIT_SUCCESS;
+}
+
+int printBugreport() {
+	uint32_t length;
+	auto* text = libnanyc_get_bugreportdetails(&length);
+	if (text) {
+		std::cout.write(text, length);
+		free(text);
+	}
+	std::cout << '\n';
+	return EXIT_SUCCESS;
+}
+
+App prepare(int argc, char** argv) {
+	App app;
+	bool version = false;
+	bool bugreport = false;
+	yuni::GetOpt::Parser options;
+	options.addParagraph("\nHelp");
+	options.addFlag(bugreport, 'b', "bugreport", "Display some useful information to report a bug");
+	options.addFlag(version, ' ', "version", "Print the version");
+	if (not options(argc, argv)) {
+		if (options.errors())
+			throw std::runtime_error("Abort due to error");
+		throw EXIT_SUCCESS;
+	}
+	if (unlikely(version))
+		throw printVersion();
+	if (unlikely(bugreport))
+		throw printBugreport();
+	return app;
+}
+
+} // namespace
+} // namespace unittests
+} // namespace ny
+
+int main(int argc, char** argv) {
+	try {
+		auto app = ny::unittests::prepare(argc, argv);
+		return EXIT_SUCCESS;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "exception: " << e.what() << '\n';
+	}
+	catch (int e) {
+		return e;
+	}
+	return EXIT_FAILURE;;
+}
