8b321e634e9c07d067cad1a8d08051927cc45079
Damien Gerard <damien@iwi.me>
2017-07-11 22:36:38 +0900

    intrinsic: remove unused compiler intrinsic

--- bootstrap/libnanyc/details/intrinsic/std.core.string.cpp
+++ bootstrap/libnanyc/details/intrinsic/std.core.string.cpp
@@ -3,11 +3,6 @@
 
 using namespace Yuni;
 
-static void nanyc_cout(nyvmthread_t* tctx, void* string, uint32_t length) {
-	assert(length == 0 or string != nullptr);
-	tctx->cout.write(&tctx->cout, (const char*) string, length);
-}
-
 template<class T> struct IntCast    { using value = T;        };
 template<> struct IntCast<int8_t>   { using value = int32_t;  };
 template<> struct IntCast<uint8_t>  { using value = uint32_t; };
@@ -44,7 +39,6 @@ void string(ny::intrinsic::Catalog& intrinsics) {
 	intrinsics.emplace("__nanyc.string.append.f32",  nanyc_string_append<float>);
 	intrinsics.emplace("__nanyc.string.append.f64",  nanyc_string_append<double>);
 	intrinsics.emplace("__nanyc.string.append.ptr",  nanyc_string_append_ptr);
-	intrinsics.emplace("__nanyc.cout", nanyc_cout);
 }
 
 } // namespace import
